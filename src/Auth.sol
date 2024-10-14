1327f65c37855ee3dac58b6e19d6fcf5a6d7b924
John Doe <bulk.yard@protonmail.com>
2024-10-14 12:46:17 +0200

    Add dependencies (#13)
    * Revert "Add liquidity-pools as dependency (#6)"
    
    This reverts commit 9949e39795fe03df42021928aed4253359715495.
    
    * fix: Remove unnecessary dep
    
    * fix: Disable ci coveragE
    
    * chore: copy mathlib from lp repo
    
    * chore: Add IAuth and Auth
    
    * chore: Enable Coverage again
    
    * chore: Decrease coverage limit. Must be increased later.
    
    ---------
    
    Co-authored-by: William Freudenberger <w.freude@icloud.com>

---
+++ src/Auth.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+import {IAuth} from "src/interfaces/IAuth.sol";
+
+/// @title  Auth
+/// @notice Simple authentication pattern
+/// @author Based on code from https://github.com/makerdao/dss
+abstract contract Auth is IAuth {
+    /// @inheritdoc IAuth
+    mapping(address => uint256) public wards;
+
+    constructor(address initialWard) {
+        wards[initialWard] = 1;
+        emit Rely(initialWard);
+    }
+
+    /// @dev Check if the msg.sender has permissions
+    modifier auth() {
+        require(wards[msg.sender] == 1, "Auth/not-authorized");
+        _;
+    }
+
+    /// @inheritdoc IAuth
+    function rely(address user) external auth {
+        wards[user] = 1;
+        emit Rely(user);
+    }
+
+    /// @inheritdoc IAuth
+    function deny(address user) external auth {
+        wards[user] = 0;
+        emit Deny(user);
+    }
+}
