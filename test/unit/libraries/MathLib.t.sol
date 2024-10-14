ce3346f15fc1ee792e57afc27830246543e36cc8
William Freudenberger <w.freude@icloud.com>
2024-10-14 16:43:47 +0200

    tests: fuzz rpow

--- test/unit/libraries/MathLib.t.sol
+++ test/unit/libraries/MathLib.t.sol
@@ -7,19 +7,24 @@ import "src/libraries/MathLib.sol";
 contract MathLibTest is Test {
     using MathLib for uint256;
 
-    // TODO(@wischli): Fuzzing
-    function testRpow() public pure {
-        uint256 base = 10 ** 27;
-        uint256 x = 2 * 10 ** 27;
-        uint256 n = 3;
+    function testRpow(uint256 x, uint256 n) public pure {
+        vm.assume(x > 1);
+        vm.assume(x <= 10 ** 12);
+        vm.assume(n <= 10 ** 4);
 
+        uint256 base = 10 ** 18;
         uint256 result = MathLib.rpow(x, n, base);
-        uint256 expected = 8 * 10 ** 27; // 2^3 = 8, scaled by base
+
+        uint256 expected = base;
+        for (uint256 i = 0; i < n; i++) {
+            require(expected <= type(uint256).max / x, "Expected value overflow");
+            expected = (expected * x + (base / 2)) / base;
+        }
 
         assertEq(result, expected, "Incorrect rpow calculation");
     }
 
-    function testMulDivDown(uint256 x, uint256 y, uint256 denominator) public {
+    function testMulDivDown(uint256 x, uint256 y, uint256 denominator) public pure {
         // Ignore cases where x * y overflows or denominator is 0.
         unchecked {
             if (denominator == 0 || (x != 0 && (x * y) / x != y)) return;
@@ -33,7 +38,7 @@ contract MathLibTest is Test {
         MathLib.mulDiv(x, y, 0, MathLib.Rounding.Down);
     }
 
-    function testMulDivUp(uint256 x, uint256 y, uint256 denominator) public {
+    function testMulDivUp(uint256 x, uint256 y, uint256 denominator) public pure {
         denominator = bound(denominator, 1, type(uint256).max - 1);
         y = bound(y, 1, type(uint256).max);
         x = bound(x, 0, (type(uint256).max - denominator - 1) / y);
@@ -53,7 +58,7 @@ contract MathLibTest is Test {
         MathLib.mulDiv(x, y, 0, MathLib.Rounding.Up);
     }
 
-    function testToUint128(uint256 x) public {
+    function testToUint128(uint256 x) public pure {
         x = bound(x, 0, type(uint128).max);
 
         assertEq(x, uint256(MathLib.toUint128(x)));
@@ -65,7 +70,7 @@ contract MathLibTest is Test {
         MathLib.toUint128(uint256(type(uint128).max) + x);
     }
 
-    function testToUint8(uint256 x) public {
+    function testToUint8(uint256 x) public pure {
         x = bound(x, 0, type(uint8).max);
 
         assertEq(x, uint256(MathLib.toUint8(x)));
