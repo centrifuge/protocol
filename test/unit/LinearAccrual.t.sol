8bb1b69bd650c42a52d5b6fee20897833d877674
William Freudenberger <w.freude@icloud.com>
2024-10-15 08:49:19 +0200

    tests: add fuzzing to LinearAccrual

--- test/unit/LinearAccrual.t.sol
+++ test/unit/LinearAccrual.t.sol
@@ -8,11 +8,6 @@ import "src/Compounding.sol";
 
 contract LinearAccrualTest is Test {
     LinearAccrual linearAccrual;
-    // TODO: Explore replacing with random rate
-    /// @dev 1.9753462 with 27 decimal precision
-    uint128 constant RATE_128 = 19_753_462 * 10 ** 11;
-    /// @dev 1.9753462 with 27 decimal precision
-    uint256 constant RATE_256 = uint256(RATE_128);
 
     event NewRateId(bytes32 rateId, uint128 rate, CompoundingPeriod period);
     event RateAccumulated(bytes32 rateId, uint128 rate, uint256 periodsPassed);
@@ -22,8 +17,9 @@ contract LinearAccrualTest is Test {
         vm.warp(1 days);
     }
 
-    function testGetRateId() public {
-        uint128 rate = 5 * 10 ** 18;
+    function testGetRateId(uint128 rate) public {
+        rate = uint128(bound(rate, 10 ** 10, 10 ** 20));
+
         // Compounding schedule irrelevant since test does not require dripping
         CompoundingPeriod period = CompoundingPeriod.Quarterly;
         vm.expectEmit(true, true, true, true);
@@ -39,26 +35,26 @@ contract LinearAccrualTest is Test {
         assertEq(uint256(lastUpdated), block.timestamp, "Last updated mismatch");
     }
 
-    function testIncreaseNormalizedDebt() public {
-        uint128 rate = 5 * 10 ** 18;
+    function testIncreaseNormalizedDebt(uint128 rate, uint128 prevNormalizedDebt, uint128 debtIncrease) public {
+        rate = uint128(bound(rate, 10 ** 10, 10 ** 20));
+        debtIncrease = uint128(bound(debtIncrease, 0, 10 ** 20));
+        vm.assume(prevNormalizedDebt < type(uint128).max - debtIncrease / rate);
+
         // Compounding schedule irrelevant since test does not require dripping
         bytes32 rateId = linearAccrual.getRateId(rate, CompoundingPeriod.Quarterly);
-
-        uint128 prevNormalizedDebt = 100 * 10 ** 18;
-        uint128 debtIncrease = 50 * 10 ** 18;
 
         uint128 newDebt = linearAccrual.increaseNormalizedDebt(rateId, prevNormalizedDebt, debtIncrease);
         uint128 expectedDebt = prevNormalizedDebt + (debtIncrease / rate);
         assertEq(newDebt, expectedDebt, "Incorrect new normalized debt");
     }
 
-    function testDecreaseNormalizedDebt() public {
-        uint128 rate = 5 * 10 ** 18;
+    function testDecreaseNormalizedDebt(uint128 rate, uint128 prevNormalizedDebt, uint128 debtDecrease) public {
+        rate = uint128(bound(rate, 10 ** 10, 10 ** 20));
+        debtDecrease = uint128(bound(debtDecrease, 0, 10 ** 20));
+        prevNormalizedDebt = uint128(bound(prevNormalizedDebt, debtDecrease / rate, 10 ** 20));
+
         // Compounding schedule irrelevant since test does not require dripping
         bytes32 rateId = linearAccrual.getRateId(rate, CompoundingPeriod.Quarterly);
-
-        uint128 prevNormalizedDebt = 100 * 10 ** 18;
-        uint128 debtDecrease = 20 * 10 ** 18;
 
         uint128 newDebt = linearAccrual.decreaseNormalizedDebt(rateId, prevNormalizedDebt, debtDecrease);
         uint128 expectedDebt = prevNormalizedDebt - (debtDecrease / rate);
@@ -66,15 +62,14 @@ contract LinearAccrualTest is Test {
         assertEq(newDebt, expectedDebt, "Incorrect new normalized debt");
     }
 
-    function testRenormalizeDebt() public {
-        uint128 oldRate = 5 * 10 ** 18;
-        uint128 newRate = 10 * 10 ** 18;
+    function testRenormalizeDebt(uint128 oldRate, uint128 newRate, uint128 prevNormalizedDebt) public {
+        oldRate = uint128(bound(oldRate, 10 ** 10, 10 ** 20));
+        newRate = uint128(bound(newRate, 10 ** 10, 10 ** 20));
+
         // Compounding schedule irrelevant since test does not require dripping
         CompoundingPeriod period = CompoundingPeriod.Quarterly;
         bytes32 oldRateId = linearAccrual.getRateId(oldRate, period);
         bytes32 newRateId = linearAccrual.getRateId(newRate, period);
-
-        uint128 prevNormalizedDebt = 100 * 10 ** 18;
 
         uint256 oldDebt = linearAccrual.debt(oldRateId, prevNormalizedDebt);
         uint256 expectedNewNormalizedDebt = oldDebt / newRate;
@@ -82,49 +77,43 @@ contract LinearAccrualTest is Test {
         assertEq(newNormalizedDebt, expectedNewNormalizedDebt, "Incorrect renormalized debt");
     }
 
-    function testDebtCalculation() public {
-        uint128 precision = 10 ** 18;
-        uint128 rate = 5 * precision;
+    function testDebtCalculation(uint128 rate, uint128 normalizedDebt) public {
+        uint128 precision = MathLib.toUint128(MathLib.One18);
+        rate = uint128(bound(rate, 10 ** 10, 10 ** 20));
+
         // Compounding schedule irrelevant since test does not require dripping
         bytes32 rateId = linearAccrual.getRateId(rate, CompoundingPeriod.Quarterly);
-
-        uint128 normalizedDebt = 100 * precision;
 
         uint256 currentDebt = linearAccrual.debt(rateId, normalizedDebt);
         uint256 expectedDebt = uint256(normalizedDebt) * rate / precision;
         assertEq(currentDebt, expectedDebt, "Incorrect debt calculation");
     }
 
-    function testDripUpdatesRate() public {
+    function testDripUpdatesRate(uint128 rate) public {
+        rate = uint128(bound(rate, MathLib.One18 / 1000, MathLib.One18 * 100));
         CompoundingPeriod period = CompoundingPeriod.Secondly;
-        bytes32 rateId = linearAccrual.getRateId(RATE_128, period);
+        bytes32 rateId = linearAccrual.getRateId(rate, period);
 
         // Pass zero periods
         linearAccrual.drip(rateId);
         (uint128 rateZeroPeriods,) = linearAccrual.rates(rateId);
-        assertEq(rateZeroPeriods, RATE_128, "Rate should be same after no passed periods");
+        assertEq(rateZeroPeriods, rate, "Rate should be same after no passed periods");
 
         // Pass one period
         (, uint64 initialLastUpdated) = linearAccrual.rates(rateId);
         vm.warp(initialLastUpdated + 1 seconds);
-        uint256 rateSquare = MathLib.mulDiv(RATE_256, RATE_256, MathLib.One18);
+        uint256 rateSquare = MathLib.mulDiv(uint256(rate), uint256(rate), MathLib.One18);
         vm.expectEmit(true, true, true, true);
         emit RateAccumulated(rateId, uint128(rateSquare), 1);
         linearAccrual.drip(rateId);
         (uint128 rateAfterTwoPeriods,) = linearAccrual.rates(rateId);
-        assertEq(uint256(rateAfterTwoPeriods), rateSquare, "Rate should be quadratic after one passed period");
+        assertEq(uint256(rateAfterTwoPeriods), rateSquare, "Rate should be ^2 after one passed period");
 
-        // Pass 3 more periods
-        vm.warp(block.timestamp + 3 seconds);
-        uint256 ratePow5 = MathLib.mulDiv(
-            MathLib.mulDiv(MathLib.mulDiv(rateSquare, RATE_256, MathLib.One18), RATE_256, MathLib.One18),
-            RATE_256,
-            MathLib.One18
-        );
-        vm.expectEmit(true, true, true, true);
-        emit RateAccumulated(rateId, uint128(ratePow5), 3);
+        // Pass 2 more periods
+        vm.warp(block.timestamp + 2 seconds);
+        uint256 ratePow4 = MathLib.mulDiv(rateSquare, rateSquare, MathLib.One18);
         linearAccrual.drip(rateId);
         (uint128 rateAfter4Periods,) = linearAccrual.rates(rateId);
-        assertEq(uint256(rateAfter4Periods), ratePow5, "Rate should be ^5 after four passed periods");
+        assertApproxEqAbs(uint256(rateAfter4Periods), ratePow4, 10 ** 4, "Rate should be ^4 after 3 passed periods");
     }
 }
