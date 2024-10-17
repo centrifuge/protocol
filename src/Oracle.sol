36ee801d32b9da03a4fbe0b0babfa54e57a518ff
lemunozm <lemunozm@gmail.com>
2024-10-17 11:17:23 +0200

    add testing and fix errors

--- src/Oracle.sol
+++ src/Oracle.sol
@@ -2,26 +2,33 @@
 pragma solidity = 0.8.28;
 
 import {IERC7726, IERC7726} from "src/interfaces/IERC7726.sol";
-import {IERC165} from "forge-std/interfaces/IERC165.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {MathLib} from "src/libraries/MathLib.sol";
 
 contract Oracle is IERC7726 {
+    /// @notice Dispatched when the action is not performed by the required feeder.
     error NotValidFeeder();
-    error ValueNotFound();
 
-    event Fed(address indexed base, address indexed quote, uint256 quoteAmount);
+    /// @notice Dispatched when the base/quote pair has never been feed.
+    error NeverFed();
+
+    /// @notice Emitted when the contract is feed with a new quote amount.
+    event Fed(address indexed base, address indexed quote, uint256 quoteAmount, uint64 referenceTime);
 
     struct Value {
-        /// Price of one base in quote denomination
+        /// @notice Price of one base in quote denomination
         uint256 amount;
-        /// Timestamp when the value was fed
+        /// @notice Timestamp when the value was fed
         uint64 referenceTime;
     }
 
+    /// @notice Owner of the contract able to feed new values
     address feeder;
+
+    /// @notice All fed values.
     mapping(address base => mapping(address quote => Value)) public values;
 
+    /// @dev check that only the feeder perform the action
     modifier onlyFeeder() {
         require(msg.sender == feeder, NotValidFeeder());
         _;
@@ -31,43 +38,65 @@ contract Oracle is IERC7726 {
         feeder = feeder_;
     }
 
-    /// @notice Feed the system with a new base -> quote relation.
-    /// @param base The identification of the base element
-    /// @param quote The identification of the quote element
+    /// @notice Feed the contract with a new base -> quote relation.
+    /// @param base The identification of the base element. If it corresponds to an ERC20, the internal computations
+    /// will use the attached decimals of that asset. If not 18 decimals will be used.
+    /// @param quote Same as `base` but for `quote`.
     /// @param quoteAmount The amount of 1 wei of base amount represented as quote units.
     function setQuote(address base, address quote, uint256 quoteAmount) external onlyFeeder {
-        values[base][quote] = Value(quoteAmount, uint64(block.timestamp));
+        uint64 referenceTime = uint64(block.timestamp);
+        values[base][quote] = Value(quoteAmount, referenceTime);
 
-        emit Fed(base, quote, quoteAmount);
+        emit Fed(base, quote, quoteAmount, referenceTime);
     }
 
     /// @inheritdoc IERC7726
     function getQuote(uint256 baseAmount, address base, address quote) external view returns (uint256 quoteAmount) {
         Value storage quoteValue = values[base][quote];
-        require(quoteValue.referenceTime > 0, ValueNotFound());
+        require(quoteValue.referenceTime > 0, NeverFed());
 
-        return MathLib.mulDiv(baseAmount, quoteValue.amount, 1 ** _extractDecimals(base));
+        return MathLib.mulDiv(baseAmount, quoteValue.amount, 10 ** _extractDecimals(base));
     }
 
     /// @dev extract the decimals used for the assetId
     /// - If the asset is an ERC20, then we ask the contract for its decimals
     /// - Otherwise we assume 18 decimals
     function _extractDecimals(address assetId) internal view returns (uint8) {
-        if (IERC165(assetId).supportsInterface(type(IERC20).interfaceId)) {
-            IERC20 erc20 = IERC20(assetId);
-            return erc20.decimals();
-        } else {
+        if (assetId.code.length == 0) {
             return 18;
+        } else {
+            (bool ok, bytes memory data) = assetId.staticcall(abi.encodeWithSelector(IERC20.decimals.selector));
+            if (ok) {
+                return abi.decode(data, (uint8));
+            } else {
+                return 18;
+            }
         }
     }
 }
 
 contract OracleFactory {
-    event NewOracle(address where);
+    /// @notice Emitted when a new oracle contract is deployed.
+    event NewOracleDeployed(address where);
 
-    function build(address feeder) external {
-        address deployed = address(new Oracle(feeder));
+    /// @notice Deploy a new oracle contract for an specific feeder.
+    /// @param feeder The account that will be able to fed values in the contract.
+    /// @param salt Extra bytes to generate different address for the same feeder.
+    function deploy(address feeder, bytes32 salt) external returns (Oracle) {
+        Oracle deployed = new Oracle{salt: salt}(feeder);
+        emit NewOracleDeployed(address(deployed));
 
-        emit NewOracle(deployed);
+        return deployed;
+    }
+
+    /// Retuns the deterministic contract address for a feeder.
+    /// @param feeder The account that will be able to fed values in the contract.
+    /// @param salt Extra bytes to generate different address for the same feeder.
+    function oracleAddress(address feeder, bytes32 salt) public view returns (address) {
+        bytes memory bytecode = abi.encodePacked(type(Oracle).creationCode, abi.encode(feeder));
+
+        return address(
+            uint160(uint256(keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(bytecode)))))
+        );
     }
 }
---
+++ test/unit/libraries/Oracle.t.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import "forge-std/mocks/MockERC20.sol";
+import "src/Oracle.sol";
+import "src/interfaces/IERC7726.sol";
+
+contract TestOracle is Test {
+    address constant FEEDER = address(1);
+    bytes32 constant SALT = bytes32(uint256(42));
+
+    MockERC20 ERC20_A = new MockERC20();
+
+    address CURR_A = address(ERC20_A); //ERC20 contract address => 6 decimals
+    address CURR_B = address(this); // Contract address => 18 decimals
+    address CURR_C = address(100); // Non contract address => 18 decimals
+
+    OracleFactory factory = new OracleFactory();
+
+    function setUp() public {
+        ERC20_A.initialize("", "", 6);
+    }
+
+    function testDeploy() public {
+        vm.expectEmit();
+        emit OracleFactory.NewOracleDeployed(factory.oracleAddress(FEEDER, SALT));
+
+        factory.deploy(FEEDER, SALT);
+    }
+
+    function testSetQuote() public {
+        Oracle oracle = factory.deploy(FEEDER, SALT);
+
+        vm.expectEmit();
+        vm.warp(1 days);
+        emit Oracle.Fed(CURR_A, CURR_B, 100, 1 days);
+
+        vm.prank(FEEDER);
+        oracle.setQuote(CURR_A, CURR_B, 100);
+    }
+
+    function testGetQuoteERC20() public {
+        Oracle oracle = factory.deploy(address(this), SALT);
+        oracle.setQuote(CURR_A, CURR_B, 100);
+
+        assertEq(oracle.getQuote(5 * 10 ** ERC20_A.decimals(), CURR_A, CURR_B), 500);
+    }
+
+    function testGetQuoteNonERC20Contract() public {
+        Oracle oracle = factory.deploy(address(this), SALT);
+        oracle.setQuote(CURR_B, CURR_A, 100);
+
+        assertEq(oracle.getQuote(5 * 10 ** 18, CURR_B, CURR_A), 500);
+    }
+
+    function testGetQuoteNonContract() public {
+        Oracle oracle = factory.deploy(address(this), SALT);
+        oracle.setQuote(CURR_C, CURR_A, 100);
+
+        assertEq(oracle.getQuote(5 * 10 ** 18, CURR_C, CURR_A), 500);
+    }
+}
