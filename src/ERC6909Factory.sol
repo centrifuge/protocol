ea70233a3a3eabbb5b529f44b29cc9bc6907b835
John <bulk.yard@protonmail.com>
2024-10-21 13:16:50 +0200

    test: Write test for StringLib

--- src/ERC6909Factory.sol
+++ src/ERC6909Factory.sol
@@ -7,7 +7,7 @@ import {ERC6909Collateral} from "src/ERC6909Collateral.sol";
 contract ERC6909Factory is Auth {
     constructor(address _owner) Auth(_owner) {}
 
-    function newCollateral(address owner, string calldata baseURI, bytes32 salt) public returns (address collateral) {
-        collateral = address(new ERC6909Collateral{salt: salt}(owner, baseURI));
+    function newCollateral(address owner, bytes32 salt) public returns (address collateral) {
+        collateral = address(new ERC6909Collateral{salt: salt}(owner));
     }
 }
---
+++ test/unit/libraries/StringLib.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.28;
+
+import "forge-std/Test.sol";
+import "src/libraries/StringLib.sol";
+
+contract StringLibTest is Test {
+    function testStringIsEmpty(string memory nonEmptyString) public {
+        vm.assume(keccak256(abi.encodePacked(nonEmptyString)) != StringLib.EMPTY_STRING);
+        assertTrue(StringLib.isEmpty(""));
+
+        assertFalse(StringLib.isEmpty(nonEmptyString));
+    }
+
+    function testConversionToString() public {
+        uint256 aNumber = 12345;
+        assertEq(StringLib.toString(aNumber), "12345");
+
+        aNumber = 0;
+        assertEq(StringLib.toString(aNumber), "0");
+
+        aNumber = type(uint256).max;
+        assertEq(
+            StringLib.toString(aNumber),
+            "115792089237316195423570985008687907853269984665640564039457584007913129639935"
+        );
+    }
+}
