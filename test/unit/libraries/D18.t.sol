816ac27c116d89ed5c7e1f2659c38a069bf74877
lemunozm <lemunozm@gmail.com>
2024-10-23 16:23:37 +0200

    move to types folder

--- test/unit/libraries/D18.t.sol
+++ test/unit/libraries/D18.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
-import "src/libraries/D18.sol";
+import "src/types/D18.sol";
 import "src/libraries/MathLib.sol";
 
 contract TestPortfolio is Test {
