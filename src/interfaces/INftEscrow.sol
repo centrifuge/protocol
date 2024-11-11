8689e04eebd3975ba2fc043837a8a1f3089333e3
lemunozm <lemunozm@gmail.com>
2024-11-11 11:44:55 +0100

    fix docs

--- src/interfaces/INftEscrow.sol
+++ src/interfaces/INftEscrow.sol
@@ -3,7 +3,7 @@ pragma solidity >= 0.8.0;
 
 import {IERC6909} from "src/interfaces/ERC6909/IERC6909.sol";
 
-/// Interface to handle an escrow of nfts
+/// Interface to handle an escrow of nfts.
 interface INftEscrow {
     event Locked(IERC6909 indexed source, uint256 indexed tokenId);
     event Unlocked(IERC6909 indexed source, uint256 indexed tokenId);
@@ -18,16 +18,16 @@ interface INftEscrow {
     error CanNotBeTransferred();
 
     /// @notice Lock a nft transfering one token to the contract.
-    /// No more tokens from the same `tokenId` can be sent to the escrow until it's locked.
-    /// @param source The contract or collection where identify the `tokenId`
-    /// @param tokenId The asset identification
+    /// No more tokens from the same `tokenId` can be sent to the escrow until it's unlocked.
+    /// @param source The contract or collection where identify the `tokenId`.
+    /// @param tokenId The asset identification.
     /// @param from Address allowed to transfer one token.
     function lock(IERC6909 source, uint256 tokenId, address from) external;
 
     /// @notice Unlock a nft transfering back the token from the contract.
-    /// The same `tokenId` can be locked again
-    /// @param source The contract or collection where identify the `tokenId`
-    /// @param tokenId The asset identification
+    /// The same `tokenId` can be locked again.
+    /// @param source The contract or collection where identifies the `tokenId`.
+    /// @param tokenId The asset identification.
     /// @param to Address where the token will be transfered to.
     function unlock(IERC6909 source, uint256 tokenId, address to) external;
 }
