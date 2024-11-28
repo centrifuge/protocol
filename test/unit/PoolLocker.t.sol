975198fbd864da4c2b7bd84708e48b26a7fbc433
lemunozm <lemunozm@gmail.com>
2024-11-28 11:02:11 +0100

    split multicall

--- test/unit/PoolLocker.t.sol
+++ test/unit/PoolLocker.t.sol
@@ -4,28 +4,14 @@ pragma solidity ^0.8.28;
 import "forge-std/Test.sol";
 import {PoolLocker} from "src/PoolLocker.sol";
 import {IPoolLocker} from "src/interfaces/IPoolLocker.sol";
-
-contract UserContract {
-    address testAddress;
-    uint256 public state;
-
-    constructor(address testAddress_) {
-        testAddress = testAddress_;
-    }
-
-    function setState() external returns (uint256) {
-        state = 100;
-        return 23;
-    }
-
-    function userFailMethod() external pure {
-        revert("user error");
-    }
-}
+import {IMulticall} from "src/interfaces/IMulticall.sol";
+import {Multicall} from "src/Multicall.sol";
 
 contract PoolManagerMock is PoolLocker {
     uint64 public wasUnlock;
     bool public waslock;
+
+    constructor(IMulticall multicall) PoolLocker(multicall) {}
 
     function _unlock(uint64 poolId) internal override {
         wasUnlock = poolId;
@@ -43,42 +29,21 @@ contract PoolManagerMock is PoolLocker {
 contract PoolLockerTest is Test {
     uint64 constant POOL_A = 42;
 
-    UserContract userContract = new UserContract(address(this));
-    PoolManagerMock poolManager = new PoolManagerMock();
+    Multicall multicall = new Multicall();
+    PoolManagerMock poolManager = new PoolManagerMock(multicall);
 
-    function testMultipleCustomCalls() public {
+    function testWithPoolUnlockerMethod() public {
         address[] memory targets = new address[](2);
         targets[0] = address(poolManager);
-        targets[1] = address(userContract);
 
         bytes[] memory methods = new bytes[](2);
         methods[0] = abi.encodeWithSelector(poolManager.poolRelatedMethod.selector);
-        methods[1] = abi.encodeWithSelector(userContract.setState.selector);
 
         bytes[] memory results = poolManager.execute(POOL_A, targets, methods);
         assertEq(abi.decode(results[0], (uint64)), POOL_A);
-        assertEq(abi.decode(results[1], (uint256)), 23);
 
         assertEq(poolManager.wasUnlock(), POOL_A);
         assertEq(poolManager.waslock(), true);
-        assertEq(userContract.state(), 100);
-    }
-
-    function testRevertAtError() public {
-        // Will revert the whole transaction when the first error appears
-
-        address[] memory targets = new address[](2);
-        targets[0] = address(userContract);
-        targets[1] = address(userContract);
-
-        bytes[] memory methods = new bytes[](2);
-        methods[0] = abi.encodeWithSelector(userContract.setState.selector);
-        methods[1] = abi.encodeWithSelector(userContract.userFailMethod.selector);
-
-        vm.expectRevert("user error");
-        poolManager.execute(POOL_A, targets, methods);
-
-        assertEq(userContract.state(), 0);
     }
 
     function testErrPoolAlreadyUnlocked() public {
@@ -95,14 +60,6 @@ contract PoolLockerTest is Test {
         methods[0] = abi.encodeWithSelector(poolManager.execute.selector, POOL_A, innerTargets, innerMethods);
 
         vm.expectRevert(IPoolLocker.PoolAlreadyUnlocked.selector);
-        poolManager.execute(POOL_A, targets, methods);
-    }
-
-    function testErrWrongExecutionParams() public {
-        address[] memory targets = new address[](1);
-        bytes[] memory methods = new bytes[](2);
-
-        vm.expectRevert(IPoolLocker.WrongExecutionParams.selector);
         poolManager.execute(POOL_A, targets, methods);
     }
 
