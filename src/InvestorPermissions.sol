7f7092ce53fa1d43a49822f59cd1233428e9d8e6
William Freudenberger <w.freude@icloud.com>
2024-12-11 16:06:21 +0100

    feat: add InvestorPermissions

---
+++ src/InvestorPermissions.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import {IInvestorPermissions} from "src/interfaces/IInvestorPermissions.sol";
+
+struct InvestorPermission {
+    uint64 validUntil;
+    bool frozen;
+}
+
+contract InvestorPermissions is IInvestorPermissions {
+    // @dev: Value for until is arbitrary because we ignore it for now but need to distinguish between default and
+    // non-default storage entries
+    uint64 private constant _DEFAUL_VALIDITY = type(uint64).min;
+
+    mapping(bytes16 shareClassId => mapping(address user => InvestorPermission)) permissions;
+
+    function add(bytes16 shareClassId, address target) external {
+        permissions[shareClassId][target] = InvestorPermission(_DEFAUL_VALIDITY, false);
+
+        emit IInvestorPermissions.Added(shareClassId, target);
+    }
+
+    function remove(bytes16 shareClassId, address target) external {
+        delete permissions[shareClassId][target];
+
+        emit IInvestorPermissions.Removed(shareClassId, target);
+    }
+
+    function freeze(bytes16 shareClassId, address target) external {
+        InvestorPermission storage perm = permissions[shareClassId][target];
+
+        require(perm.validUntil == _DEFAUL_VALIDITY, Missing());
+        require(perm.frozen == false, AlreadyFrozen());
+
+        perm.frozen = true;
+
+        emit IInvestorPermissions.Frozen(shareClassId, target);
+    }
+
+    function unfreeze(bytes16 shareClassId, address target) external {
+        InvestorPermission storage perm = permissions[shareClassId][target];
+
+        require(perm.validUntil == _DEFAUL_VALIDITY, Missing());
+        require(perm.frozen == true, NotFrozen());
+
+        permissions[shareClassId][target].frozen = false;
+
+        emit IInvestorPermissions.Unfrozen(shareClassId, target);
+    }
+
+    function isFrozenInvestor(bytes16 shareClassId, address target) public view returns (bool) {
+        InvestorPermission memory perm = permissions[shareClassId][target];
+
+        return perm.validUntil == _DEFAUL_VALIDITY && perm.frozen == true;
+    }
+
+    function isUnfrozenInvestor(bytes16 shareClassId, address target) public view returns (bool) {
+        InvestorPermission memory perm = permissions[shareClassId][target];
+        return perm.validUntil == _DEFAUL_VALIDITY && perm.frozen == false;
+    }
+}
