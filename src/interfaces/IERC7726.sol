059ec53fa95810a941ab613e3cf4b8812c7b3fcc
lemunozm <lemunozm@gmail.com>
2024-12-12 09:19:23 +0100

    change docs

--- src/interfaces/IERC7726.sol
+++ src/interfaces/IERC7726.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.6.2;
 /// [ERC-7726](https://eips.ethereum.org/EIPS/eip-7726): Common Quote Oracle
 /// Interface for asset conversions.
 interface IERC7726 {
-    /// @notice Returns the value of `baseAmount` of `base` in `quote` terms.
-    /// @param base The asset the user provides the amount.
-    /// @param quote The asset that the user needs to know the value for.
-    /// @param baseAmount An amount of `base` in `base` terms.
+    /// @notice Returns the value of baseAmount of base in quote terms, e.g. 10 ETH (base) in USDC (quote).
+    /// @param base The asset in which the baseAmount is denominated in
+    /// @param quote The asset in which the user needs to value the baseAmount
+    /// @param baseAmount The amount of base in base terms.
     function getQuote(uint256 baseAmount, address base, address quote) external view returns (uint256 quoteAmount);
 }
