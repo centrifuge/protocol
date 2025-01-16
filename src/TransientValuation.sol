63dec32880c166222363e7b86903c7f9cd64eaa8
lemunozm <lemunozm@gmail.com>
2025-01-16 12:09:57 +0100

    add Conversion library

--- src/TransientValuation.sol
+++ src/TransientValuation.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {IERC7726} from "src/interfaces/IERC7726.sol";
 import {AssetId} from "src/types/AssetId.sol";
 import {D18} from "src/types/D18.sol";
-import {MathLib} from "src/libraries/MathLib.sol";
+
+import {Conversion} from "src/libraries/Conversion.sol";
+
+import {IERC7726} from "src/interfaces/IERC7726.sol";
 import {ITransientValuation} from "src/interfaces/ITransientValuation.sol";
-import {IERC20Metadata} from "src/interfaces/IERC20Metadata.sol";
 
 contract TransientValuation is ITransientValuation {
-    using MathLib for uint256;
-
-    /// Temporal price set and used to obtain the quote.
+    /// @notice Temporal price set and used to obtain the quote.
     D18 public /*TODO: transient*/ price;
 
     /// @inheritdoc ITransientValuation
@@ -21,13 +20,6 @@ contract TransientValuation is ITransientValuation {
 
     /// @inheritdoc IERC7726
     function getQuote(uint256 baseAmount, address base, address quote) external view returns (uint256 quoteAmount) {
-        uint8 baseDecimals = IERC20Metadata(base).decimals();
-        uint8 quoteDecimals = IERC20Metadata(quote).decimals();
-
-        if (baseDecimals == quoteDecimals) {
-            return price.mulUint256(baseAmount);
-        }
-
-        return price.mulUint256(MathLib.mulDiv(baseAmount, 10 ** quoteDecimals, 10 ** baseDecimals));
+        return Conversion.convertWithPrice(baseAmount, base, quote, price);
     }
 }
