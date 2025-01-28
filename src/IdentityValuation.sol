fd91dd6856d6b6d35674f1f92135b89375cdecf9
lemunozm <lemunozm@gmail.com>
2025-01-28 12:20:36 +0100

    adapt to BaseValuation

--- src/IdentityValuation.sol
+++ src/IdentityValuation.sol
@@ -7,10 +7,15 @@ import {Conversion} from "src/libraries/Conversion.sol";
 
 import {IERC7726, IERC7726Ext} from "src/interfaces/IERC7726.sol";
 import {IIdentityValuation} from "src/interfaces/IIdentityValuation.sol";
+import {IAssetManager} from "src/interfaces/IAssetManager.sol";
 
-contract IdentityValuation is IIdentityValuation {
+import {BaseValuation} from "src/BaseValuation.sol";
+
+contract IdentityValuation is BaseValuation, IIdentityValuation {
+    constructor(IAssetManager assetManager, address deployer) BaseValuation(assetManager, deployer) {}
+
     /// @inheritdoc IERC7726
     function getQuote(uint256 baseAmount, address base, address quote) external view returns (uint256 quoteAmount) {
-        return Conversion.convertWithPrice(baseAmount, base, quote, d18(1e18));
+        return Conversion.convertWithPrice(baseAmount, _getDecimals(base), _getDecimals(quote), d18(1e18));
     }
 }
--- test/unit/IdentityValuation.t.sol
+++ test/unit/IdentityValuation.t.sol
@@ -7,33 +7,29 @@ import {D18, d18} from "src/types/D18.sol";
 import {MathLib} from "src/libraries/MathLib.sol";
 import {IdentityValuation} from "src/IdentityValuation.sol";
 import {IERC20Metadata} from "src/interfaces/IERC20Metadata.sol";
+import {IAssetManager} from "src/interfaces/IAssetManager.sol";
 
-contract C18 {
-    function decimals() external pure returns (uint8) {
-        return 18;
-    }
-}
+address constant C6 = address(6);
+address constant C18 = address(18);
 
-contract C6 {
-    function decimals() external pure returns (uint8) {
-        return 6;
+contract MockedAssetManager {
+    function decimals(uint256 tokenId) external pure returns (uint8) {
+        return uint8(tokenId);
     }
 }
 
 contract TestIdentityValuation is Test {
-    address c18 = address(new C18());
-    address c6 = address(new C6());
-    IdentityValuation valuation = new IdentityValuation();
+    IdentityValuation valuation = new IdentityValuation(IAssetManager(address(new MockedAssetManager())), address(0));
 
     function testSameDecimals() public view {
-        assertEq(valuation.getQuote(100 * 1e6, c6, c6), 100 * 1e6);
+        assertEq(valuation.getQuote(100 * 1e6, C6, C6), 100 * 1e6);
     }
 
     function testFromMoreDecimalsToLess() public view {
-        assertEq(valuation.getQuote(100 * 1e18, c18, c6), 100 * 1e6);
+        assertEq(valuation.getQuote(100 * 1e18, C18, C6), 100 * 1e6);
     }
 
     function testFromLessDecimalsToMore() public view {
-        assertEq(valuation.getQuote(100 * 1e6, c6, c18), 100 * 1e18);
+        assertEq(valuation.getQuote(100 * 1e6, C6, C18), 100 * 1e18);
     }
 }
