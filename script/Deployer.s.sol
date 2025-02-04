c932490ce73f4261f1054d0b3d0e0830142b9665
lemunozm <lemunozm@gmail.com>
2025-02-04 16:39:07 +0100

    cleanings and fixes

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -3,14 +3,15 @@ pragma solidity 0.8.28;
 
 import "forge-std/Script.sol";
 
+import {AssetId, newAssetIdFromISO4217} from "src/types/AssetId.sol";
+
 import {TransientValuation} from "src/TransientValuation.sol";
 import {OneToOneValuation} from "src/OneToOneValuation.sol";
 import {PoolRegistry} from "src/PoolRegistry.sol";
 import {Multicall} from "src/Multicall.sol";
 import {SingleShareClass} from "src/SingleShareClass.sol";
 import {Holdings} from "src/Holdings.sol";
-//import {AssetManager} from "src/AssetManager.sol";
-import {IAssetManager} from "src/interfaces/IAssetManager.sol"; // TODO: remove import
+import {AssetManager} from "src/AssetManager.sol";
 //import {Accounting} from "src/Accounting.sol";
 import {IAccounting} from "src/interfaces/IAccounting.sol"; // TODO: remove import
 //import {Gateway} from "src/Gateway.sol";
@@ -25,35 +26,59 @@ contract Deployer is Script {
     // Core contracts
     Multicall public multicall;
     PoolRegistry public poolRegistry;
-    IAssetManager public assetManager = IAssetManager(address(0));
+    AssetManager public assetManager;
     IAccounting public accounting = IAccounting(address(0));
     Holdings public holdings;
     SingleShareClass public singleShareClass;
     PoolManager public poolManager;
     IGateway public gateway = IGateway(address(0));
 
-    function deploy(address deployer) public {
-        // TODO: new AssetRegistry()
-        // TODO: new Accounting()
-        // TODO: new Gateway()
+    // Data
+    AssetId immutable USD = newAssetIdFromISO4217(840);
 
-        transientValuation = new TransientValuation(assetManager, deployer);
-        oneToOneValuation = new OneToOneValuation(assetManager, deployer);
-
-        poolRegistry = new PoolRegistry(deployer);
-        holdings = new Holdings(poolRegistry, deployer);
-        singleShareClass = new SingleShareClass(poolRegistry, deployer);
-
+    function deploy() public {
         multicall = new Multicall();
-        poolManager = new PoolManager(multicall, poolRegistry, assetManager, accounting, holdings, gateway, deployer);
 
+        transientValuation = new TransientValuation(assetManager, address(this));
+        oneToOneValuation = new OneToOneValuation(assetManager, address(this));
+
+        poolRegistry = new PoolRegistry(address(this));
+        assetManager = new AssetManager(address(this));
+        // TODO: initialize Accounting
+        holdings = new Holdings(poolRegistry, address(this));
+
+        singleShareClass = new SingleShareClass(poolRegistry, address(this));
+        poolManager =
+            new PoolManager(multicall, poolRegistry, assetManager, accounting, holdings, gateway, address(this));
+        // TODO: initialize Gateway
+
+        _rely();
+        _initialConfig();
+    }
+
+    function _rely() private {
         poolRegistry.rely(address(poolManager));
-        //assetManager.rely(address(poolManager));
+        assetManager.rely(address(poolManager));
         holdings.rely(address(poolManager));
         //accounting.rely(address(accounting));
         singleShareClass.rely(address(poolManager));
-        //gateway.rely(address(poolManager));
-
         poolManager.rely(address(gateway));
+        //gateway.rely(address(poolManager));
+    }
+
+    function _initialConfig() private {
+        assetManager.registerAsset(USD, "United States dollar", "USD", 18);
+    }
+
+    function removeDeployerAccess() public {
+        transientValuation.deny(address(this));
+        oneToOneValuation.deny(address(this));
+        poolRegistry.deny(address(this));
+        assetManager.deny(address(this));
+        // TODO: deny accounting
+        holdings.deny(address(this));
+        singleShareClass.deny(address(this));
+        poolManager.deny(address(this));
+        // TODO: deny gateway
     }
 }
--- test/integration/Cases.t.sol
+++ test/integration/Cases.t.sol
@@ -10,6 +10,7 @@ import {D18, d18} from "src/types/D18.sol";
 import {shareClassIdFor} from "src/SingleShareClass.sol";
 
 import {AccountType} from "src/interfaces/IPoolManager.sol";
+import {IMulticall} from "src/interfaces/IMulticall.sol";
 
 import {Deployer} from "script/Deployer.s.sol";
 
@@ -21,23 +22,25 @@ contract TestCommon is Deployer, Test {
     uint32 constant CHAIN_1 = 1;
     uint32 constant CHAIN_2 = 2;
 
-    address immutable DEPLOYER = makeAddr("Deployer");
-    address immutable FM = makeAddr("Fund manager");
-    address immutable ANY = makeAddr("Anyone without role");
-    bytes32 immutable INVESTOR = bytes32(uint256(uint160(makeAddr("Investor"))));
+    address immutable FM = makeAddr("FM");
+    address immutable ANY = makeAddr("Anyone");
+    bytes32 immutable INVESTOR = bytes32("Investor");
 
-    AssetId immutable USD = newAssetIdFromISO4217(840);
     AssetId immutable USDC_C2 = newAssetId(CHAIN_2, 1);
 
-    MockCentrifugeVaults cv = new MockCentrifugeVaults(poolManager);
+    MockCentrifugeVaults cv;
 
     function setUp() public {
-        deploy(DEPLOYER);
+        // Deployment
+        deploy();
 
-        // TODO: remove this line when Gateway is implemented
-        poolManager.rely(address(cv));
+        cv = new MockCentrifugeVaults(poolManager);
 
-        // Label contracts (for debugging)
+        poolManager.rely(address(cv)); // TODO: remove this the Gateway is implemented.
+
+        removeDeployerAccess();
+
+        // Label contracts & actors (for debugging)
         vm.label(address(transientValuation), "TransientValuation");
         vm.label(address(oneToOneValuation), "OneToOneValuation");
         vm.label(address(multicall), "Multicall");
@@ -48,18 +51,20 @@ contract TestCommon is Deployer, Test {
         vm.label(address(singleShareClass), "SingleShareClass");
         vm.label(address(poolManager), "PoolManager");
         vm.label(address(gateway), "Gateway");
+        vm.label(address(cv), "CV");
 
         // We decide CP is located at CHAIN_1 for messaging
         vm.chainId(CHAIN_1);
+    }
 
-        // Initial deployed configuration
-        vm.prank(DEPLOYER);
-        assetManager.registerAsset(USD, "United States dollar", "USD", 18);
+    /// Creates a PoolMananger call
+    function _createCall(bytes memory encoding) internal view returns (IMulticall.Call memory) {
+        return IMulticall.Call(address(poolManager), encoding);
     }
 }
 
 contract TestConfiguration is TestCommon {
-    function configurePool() public returns (PoolId poolId, ShareClassId scId) {
+    function testBaseConfigurationPool() public returns (PoolId poolId, ShareClassId scId) {
         cv.registerAsset(USDC_C2, "USD Coin", "USDC", 6);
 
         vm.prank(FM);
@@ -73,37 +78,27 @@ contract TestConfiguration is TestCommon {
         accounts[2] = AccountId.wrap(0x100 | uint8(AccountType.LOSS));
         accounts[3] = AccountId.wrap(0x100 | uint8(AccountType.GAIN));
 
-        address[] memory targets = new address[](7);
-        targets[0] = address(poolManager);
-        targets[1] = address(poolManager);
-        targets[2] = address(poolManager);
-        targets[3] = address(poolManager);
-        targets[4] = address(poolManager);
-        targets[5] = address(poolManager);
-        targets[6] = address(poolManager);
-        targets[7] = address(poolManager);
-        targets[8] = address(poolManager);
-        targets[9] = address(poolManager);
-        targets[10] = address(poolManager);
-        targets[11] = address(poolManager);
+        uint256 i;
+        IMulticall.Call[] memory calls = new IMulticall.Call[](12);
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.setPoolMetadata.selector, bytes("Testing pool")));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.addShareClass.selector, bytes("")));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.notifyPool.selector, CHAIN_2));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.notifyShareClass.selector, CHAIN_2, scId));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.allowHoldingAsset.selector, USDC_C2, true));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.createAccount.selector, accounts[0], true));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.createAccount.selector, accounts[1], false));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.createAccount.selector, accounts[2], false));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.createAccount.selector, accounts[3], true));
+        calls[i++] = _createCall(
+            abi.encodeWithSelector(poolManager.createHolding.selector, scId, USDC_C2, oneToOneValuation, accounts)
+        );
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.allowInvestorAsset.selector, USDC_C2, true));
+        calls[i++] = _createCall(abi.encodeWithSelector(poolManager.notifyAllowedAsset.selector, scId, USDC_C2));
 
-        bytes[] memory methods = new bytes[](12);
-        methods[0] = abi.encodeWithSelector(poolManager.setPoolMetadata.selector, bytes("Testing pool"));
-        methods[1] = abi.encodeWithSelector(poolManager.addShareClass.selector, bytes(""));
-        methods[2] = abi.encodeWithSelector(poolManager.notifyPool.selector, CHAIN_2);
-        methods[3] = abi.encodeWithSelector(poolManager.notifyShareClass.selector, CHAIN_2, scId);
-        methods[4] = abi.encodeWithSelector(poolManager.allowHoldingAsset.selector, USDC_C2, true);
-        methods[5] = abi.encodeWithSelector(poolManager.createAccount.selector, accounts[0], true);
-        methods[6] = abi.encodeWithSelector(poolManager.createAccount.selector, accounts[1], false);
-        methods[7] = abi.encodeWithSelector(poolManager.createAccount.selector, accounts[2], false);
-        methods[8] = abi.encodeWithSelector(poolManager.createAccount.selector, accounts[3], true);
-        methods[9] =
-            abi.encodeWithSelector(poolManager.createHolding.selector, scId, USDC_C2, oneToOneValuation, accounts);
-        methods[10] = abi.encodeWithSelector(poolManager.allowInvestorAsset.selector, USDC_C2, true);
-        methods[11] = abi.encodeWithSelector(poolManager.notifyAllowedAsset.selector, USDC_C2);
+        assertEq(i, calls.length);
 
         vm.prank(FM);
-        poolManager.execute(poolId, targets, methods);
+        poolManager.execute(poolId, calls);
 
         // From this point, pool is ready for investing from CV side
     }
@@ -114,8 +109,8 @@ contract TestInvesting is TestConfiguration {
     D18 immutable PERCENT_20 = d18(1, 5);
     D18 immutable NAV_PER_SHARE = d18(2, 1);
 
-    function baseFlow() public {
-        (PoolId poolId, ShareClassId scId) = configurePool();
+    function testBaseFlow() public {
+        (PoolId poolId, ShareClassId scId) = testBaseConfigurationPool();
 
         cv.requestDeposit(
             poolId,
@@ -125,16 +120,12 @@ contract TestInvesting is TestConfiguration {
             INVESTOR_AMOUNT ** assetManager.decimals(USDC_C2.raw())
         );
 
-        address[] memory targets = new address[](2);
-        targets[0] = address(poolManager);
-        targets[1] = address(poolManager);
-
-        bytes[] memory methods = new bytes[](2);
-        methods[0] = abi.encodeWithSelector(poolManager.approveDeposits.selector, scId, USDC_C2, PERCENT_20);
-        methods[1] = abi.encodeWithSelector(poolManager.issueShares.selector, scId, USDC_C2, NAV_PER_SHARE);
+        IMulticall.Call[] memory calls = new IMulticall.Call[](2);
+        calls[0] = _createCall(abi.encodeWithSelector(poolManager.approveDeposits.selector, scId, USDC_C2, PERCENT_20));
+        calls[1] = _createCall(abi.encodeWithSelector(poolManager.issueShares.selector, scId, USDC_C2, NAV_PER_SHARE));
 
         vm.prank(FM);
-        poolManager.execute(poolId, targets, methods);
+        poolManager.execute(poolId, calls);
 
         vm.prank(ANY);
         poolManager.claimDeposit(poolId, scId, USDC_C2, INVESTOR);
