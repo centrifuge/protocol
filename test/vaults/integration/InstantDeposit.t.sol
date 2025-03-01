e09882cf60db2ce5ccc47c24fc802e0c48de6886
Jeroen Offerijns <jeroen@offerijns.com>
2025-03-01 19:36:36 +0100

    Expand test

--- test/vaults/integration/InstantDeposit.t.sol
+++ test/vaults/integration/InstantDeposit.t.sol
@@ -39,6 +39,8 @@ contract DepositTest is BaseTest {
             address(instantManager)
         );
         root.relyContract(address(tranche), address(instantManager));
+        root.relyContract(address(tranche), address(vault));
+        root.relyContract(address(investmentManager), address(vault));
 
         // Check price and max amounts
         assertEq(vault.previewMint(amount / 2), amount);
@@ -46,12 +48,12 @@ contract DepositTest is BaseTest {
         assertEq(vault.maxDeposit(self), type(uint256).max);
         assertEq(vault.maxMint(self), type(uint256).max);
 
-        // will fail - user did not give asset allowance to vault
+        // Will fail - user did not give asset allowance to vault
         vm.expectRevert(SafeTransferLib.SafeTransferFromFailed.selector);
         vault.deposit(amount, self);
         erc20.approve(address(vault), amount);
 
-        // will fail - user not member: can not send funds
+        // Will fail - user not member: can not send funds
         vm.expectRevert(bytes("RestrictionManager/transfer-blocked"));
         vault.deposit(amount, self);
 
@@ -59,7 +61,7 @@ contract DepositTest is BaseTest {
         centrifugeChain.updateMember(vault.poolId(), vault.trancheId(), self, type(uint64).max); // add user as member
         assertEq(vault.isPermissioned(self), true);
 
-        // will fail - investment asset not allowed
+        // Will fail - investment asset not allowed
         centrifugeChain.disallowAsset(vault.poolId(), defaultAssetId);
         vm.expectRevert(IInstantManager.AssetNotAllowed.selector);
         vault.deposit(amount, self);
@@ -67,11 +69,18 @@ contract DepositTest is BaseTest {
         assertEq(tranche.balanceOf(self), 0);
         assertEq(erc20.balanceOf(self), amount);
 
-        // success
+        // Success
         centrifugeChain.allowAsset(vault.poolId(), defaultAssetId);
         vault.deposit(amount, self);
 
         assertEq(erc20.balanceOf(self), 0);
         assertEq(tranche.balanceOf(self), amount / 2);
+
+        // Can now request redemption through async vault
+        // TODO: doesn't work yet becuase investments in investment mgr are indexed by the async vault address
+        // assertEq(asyncVault.pendingRedeemRequest(0, self), 0);
+        // vault.requestRedeem(amount / 2, self, self);
+
+        // assertEq(asyncVault.pendingRedeemRequest(0, self), amount / 2);
     }
 }
