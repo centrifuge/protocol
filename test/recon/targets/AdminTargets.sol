0d38f6082b4e7697de4fd1e9a9eecb5b888b4776
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-03-12 11:05:42 -0300

    chore: remove PM admin functions only callable via router

--- test/recon/targets/AdminTargets.sol
+++ test/recon/targets/AdminTargets.sol
@@ -22,97 +22,8 @@ abstract contract AdminTargets is
 
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
 
-    function poolManager_lock() public asAdmin {
-        poolManager.lock();
-    }
-
-    function poolManager_unlock(PoolId poolId, address admin) public asAdmin {
-        poolManager.unlock(poolId, admin);
-    }
-
-    function poolManager_createPool(address admin, uint32 isoCode, IShareClassManager shareClassManager) public asAdmin returns (PoolId poolId) {
-        AssetId assetId_ = newAssetId(isoCode); 
-        
-        poolId = poolManager.createPool(admin, assetId_, shareClassManager);
-
-        return poolId;
-    }
-
-    function poolManager_setAccountMetadata(AccountId account, bytes memory metadata) public asActor {
-        poolManager.setAccountMetadata(account, metadata);
-    }
-
-    function poolManager_setHoldingAccountId(ShareClassId scId, AssetId assetId, AccountId accountId) public asActor {
-        poolManager.setHoldingAccountId(scId, assetId, accountId);
-    }
-
-    function poolManager_setPoolMetadata(bytes memory metadata) public asAdmin {
-        poolManager.setPoolMetadata(metadata);
-    }
-
-    function poolManager_notifyPool(uint32 chainId) public asAdmin {
-        poolManager.notifyPool(chainId);
-    }
-
-    function poolManager_notifyShareClass(uint32 chainId, ShareClassId scId, bytes32 hook) public asAdmin {
-        poolManager.notifyShareClass(chainId, scId, hook);
-    }
-
-    function poolManager_allowPoolAdmin(address account, bool allow) public asAdmin {
-        poolManager.allowPoolAdmin(account, allow);
-    }
-
-    function poolManager_addShareClass(string memory name, string memory symbol, bytes32 salt, bytes memory data) public asAdmin {
-        poolManager.addShareClass(name, symbol, salt, data);
-    }
-
-    function poolManager_approveDeposits(ShareClassId scId, AssetId paymentAssetId, uint128 maxApproval, IERC7726 valuation) public asAdmin {
-        poolManager.approveDeposits(scId, paymentAssetId, maxApproval, valuation);
-    }
-
-    function poolManager_approveRedeems(ShareClassId scId, AssetId payoutAssetId, uint128 maxApproval) public asAdmin {
-        poolManager.approveRedeems(scId, payoutAssetId, maxApproval);
-    }   
-
-    function poolManager_issueShares(ShareClassId scId, AssetId depositAssetId, D18 navPerShare) public asAdmin {
-        poolManager.issueShares(scId, depositAssetId, navPerShare);
-    }
-
-    function poolManager_revokeShares(ShareClassId scId, AssetId payoutAssetId, D18 navPerShare, IERC7726 valuation) public asAdmin {
-        poolManager.revokeShares(scId, payoutAssetId, navPerShare, valuation);
-    }
-
-    function poolManager_createHolding(ShareClassId scId, AssetId assetId, IERC7726 valuation, uint24 prefix) public asAdmin {
-        poolManager.createHolding(scId, assetId, valuation, prefix);
-    }
-
-    function poolManager_decreaseHolding(ShareClassId scId, AssetId assetId, IERC7726 valuation, uint128 amount) public asAdmin {
-        poolManager.decreaseHolding(scId, assetId, valuation, amount);
-    }
-
-    function poolManager_increaseHolding(ShareClassId scId, AssetId assetId, IERC7726 valuation, uint128 amount) public asAdmin {
-        poolManager.increaseHolding(scId, assetId, valuation, amount);
-    }
-
-    function poolManager_updateHolding(ShareClassId scId, AssetId assetId) public asAdmin {
-        poolManager.updateHolding(scId, assetId);
-    }
-
-    function poolManager_updateHoldingValuation(ShareClassId scId, AssetId assetId, IERC7726 valuation) public asAdmin {
-        poolManager.updateHoldingValuation(scId, assetId, valuation);
-    }
-
-    function poolManager_createAccount(AccountId account, bool isDebitNormal) public asAdmin {
-        poolManager.createAccount(account, isDebitNormal);
-    }
-
-    function poolManager_addCredit(AccountId account, uint128 amount) public asAdmin {
-        poolManager.addCredit(account, amount);
-    }
-
-    function poolManager_addDebit(AccountId account, uint128 amount) public asAdmin {
-        poolManager.addDebit(account, amount);
-    }
+    // === PoolManager === //
+    /// Gateway owner methods: these get called directly because we're not using the gateway in our setup
 
     function poolManager_registerAsset(uint32 isoCode) public asAdmin {
         AssetId assetId_ = newAssetId(isoCode); 
@@ -142,10 +53,6 @@ abstract contract AdminTargets is
 
     function poolManager_cancelRedeemRequest(PoolId poolId, ShareClassId scId, bytes32 investor, AssetId payoutAssetId) public asAdmin {
         poolManager.cancelRedeemRequest(poolId, scId, investor, payoutAssetId);
-    }
-
-    function poolManager_file(bytes32 what, address data) public asAdmin {
-        poolManager.file(what, data);
     }
 
     // === SingleShareClass === //
