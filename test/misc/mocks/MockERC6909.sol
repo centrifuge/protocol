4d03f2694f8a801f34494c8e4b7b1af04f00104b
William Freudenberger <w.freude@icloud.com>
2025-03-13 17:33:07 +0100

    refactor: MockERC6909

--- test/misc/mocks/MockERC6909.sol
+++ test/misc/mocks/MockERC6909.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.28;
 import {ERC6909} from "src/misc/ERC6909.sol";
 import {IERC6909MetadataExt} from "src/misc/interfaces/IERC6909.sol";
 
-contract MockERC6909 is IERC6909MetadataExt, ERC6909 {
+contract MockERC6909 is IERC6909MetadataExt {
+    mapping(address owner => mapping(address spender => mapping(uint256 tokenId => uint256))) public allowance;
+
     function decimals(uint256 tokenId) external pure returns (uint8) {
         return uint8(tokenId);
     }
@@ -15,5 +17,10 @@ contract MockERC6909 is IERC6909MetadataExt, ERC6909 {
 
     function symbol(uint256 /*tokenId*/ ) external pure returns (string memory) {
         return "mocked symbol";
+    }
+
+    function approve(address spender, uint256 tokenId, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender][tokenId] = amount;
+        return true;
     }
 }
