12209145e94215e88b03df4da79846b314a56a80
William Freudenberger <w.freude@icloud.com>
2025-03-18 15:51:28 +0100

    feat: preview share class id (#161)
    * feat: preview share class id
    
    * refactor: add share class id constructor
    
    * refactor: uint32 index
    
    * feat: add shareClassCount to IShareClassManager

--- src/pools/types/ShareClassId.sol
+++ src/pools/types/ShareClassId.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
+import {PoolId} from "src/pools/types/PoolId.sol";
+
 type ShareClassId is bytes16;
 
 function isNull(ShareClassId scId) pure returns (bool) {
@@ -13,6 +15,10 @@ function equals(ShareClassId left, ShareClassId right) pure returns (bool) {
 
 function raw(ShareClassId scId) pure returns (bytes16) {
     return ShareClassId.unwrap(scId);
+}
+
+function newShareClassId(PoolId poolId, uint32 index) pure returns (ShareClassId scId) {
+    return ShareClassId.wrap(bytes16(uint128(PoolId.unwrap(poolId) + index)));
 }
 
 using {isNull, raw, equals as ==} for ShareClassId global;
