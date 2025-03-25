81ed836348cb665b9c4eff11811d07772c7aaf86
lemunozm <lemunozm@gmail.com>
2025-03-25 18:50:38 +0100

    add deploy script

---
+++ deployments/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Basic usage: ./development/deploy.sh <Localhost|Axelar|Wormhole>
+
+source .env
+
+ADAPTER=$1
+
+if [[ -z "$1" ]]; then
+    echo "ADAPTER is not defined"
+    exit 1
+fi
+
+mkdir -p deployments/latest
+
+case "$ADAPTER" in
+    Localhost|Axelar|Wormhole)
+        forge script script/adapters/${ADAPTER}.s.sol:${ADAPTER}Deployer --optimize --rpc-url $RPC_URL --private-key $PRIVATE_KEY --verify --broadcast --chain-id $CHAIN_ID --etherscan-api-key $ETHERSCAN_KEY
+        ;;
+    *)
+        echo "Adapter '$ADAPTER' was chosen"
+        echo "Adapter (first argument) should be one of Localhost, Axelar, Wormhole"
+        exit 1
+    ;;
+esac
+
