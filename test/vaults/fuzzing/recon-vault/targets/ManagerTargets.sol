eb58d387755511c9314f266cf533f73432c99315
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-04-07 17:08:45 -0300

    chore: cleanup

--- test/vaults/fuzzing/recon-vault/targets/ManagerTargets.sol
+++ test/vaults/fuzzing/recon-vault/targets/ManagerTargets.sol
@@ -51,4 +51,26 @@ abstract contract ManagerTargets is
         require(to != address(escrow), "Cannot mint to escrow");
         MockERC20(_getAsset()).mint(to, amt);
     }
+
+    // NOTE: These are unused because we currently just deploy and switch to entirely new system
+    // function changePool() public {
+    //     // Given Pool, swap to new pool
+    //     // Pool is easy
+    //     // But given a Pool, we need to set a Share Class and a Currency
+    //     // So we check if they exist, and use them
+
+    //     // If they don't, we still switch
+    //     // But we will need medusa to deploy a new Share (and currency)
+    // }
+
+    // // TODO: Consider adding ways to deploy
+    // // TODO: Check if it's worth having incorrect settings as a means to explore them
+
+    // // Cycle through Shares -> Changes the Share ID without changing the currency
+    // function changeShareForPool() public {}
+
+    // // Changes the Currency being used
+    // // Keeps the Same Pool
+    // // Since it changes the currency, it also changes the share class
+    // function changeCurrencyForPool() public {}
 }
