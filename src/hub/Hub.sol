91b4db96ea4a62b7f4cc5cdfbcd16f64513f62f2
Jeroen Offerijns <git@o6ns.com>
2025-04-11 19:45:00 +0200

    Fix casting in updateHolding

--- src/hub/Hub.sol
+++ src/hub/Hub.sol
@@ -573,14 +573,20 @@ contract Hub is Multicall, Auth, Recoverable, IHub, IHubGatewayHandler {
         } else if (diff < 0) {
             if (holdings.isLiability(poolId, scId, assetId)) {
                 accounting.addCredit(
-                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Expense)), uint128(diff)
+                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Expense)),
+                    uint128(uint256(-int256(diff)))
                 );
                 accounting.addDebit(
-                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Liability)), uint128(diff)
+                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Liability)),
+                    uint128(uint256(-int256(diff)))
                 );
             } else {
-                accounting.addCredit(holdings.accountId(poolId, scId, assetId, uint8(AccountType.Asset)), uint128(diff));
-                accounting.addDebit(holdings.accountId(poolId, scId, assetId, uint8(AccountType.Loss)), uint128(diff));
+                accounting.addCredit(
+                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Asset)), uint128(uint256(-int256(diff)))
+                );
+                accounting.addDebit(
+                    holdings.accountId(poolId, scId, assetId, uint8(AccountType.Loss)), uint128(uint256(-int256(diff)))
+                );
             }
         }
     }
