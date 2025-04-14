10078e80e13bbb706d90ba89e7193963ac709686
Jeroen Offerijns <git@o6ns.com>
2025-04-14 11:14:10 +0200

    Fix libraries

--- script/adapters/Wormhole.s.sol
+++ script/adapters/Wormhole.s.sol
@@ -4,20 +4,24 @@ pragma solidity 0.8.28;
 import {WormholeAdapter} from "src/common/WormholeAdapter.sol";
 import {ISafe} from "src/common/interfaces/IGuardian.sol";
 
-import "forge-std/Script.sol";
+import {FullDeployer, HubDeployer, VaultsDeployer} from "script/FullDeployer.s.sol";
 
-// Script to deploy a Wormhole Adapter.
-contract WormholeDeployer is Script {
+// Script to deploy CP and CP with an Wormhole Adapter.
+contract WormholeDeployer is FullDeployer {
     function run() public {
+        uint16 localCentrifugeId = uint16(vm.envUint("CENTRIFUGE_ID"));
         uint16 remoteCentrifugeId = uint16(vm.envUint("REMOTE_CENTRIFUGE_ID"));
         address relayer = address(vm.envAddress("WORMHOLE_RELAYER"));
         uint16 localWormholeId = uint16(vm.envUint("WORMHOLE_LOCAL_CHAIN_ID"));
 
         vm.startBroadcast();
 
-        WormholeAdapter adapter = new WormholeAdapter(gateway, relayer, localWormholeId, msg.sender);
+        deployFull(localCentrifugeId, ISafe(vm.envAddress("ADMIN")), msg.sender);
 
-        guardian.setAdapter();
+        WormholeAdapter adapter = new WormholeAdapter(gateway, relayer, localWormholeId, msg.sender);
+        wire(remoteCentrifugeId, adapter, msg.sender);
+
+        removeFullDeployerAccess(msg.sender);
 
         vm.stopBroadcast();
     }
