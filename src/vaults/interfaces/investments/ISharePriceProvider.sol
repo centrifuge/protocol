f40cae60c4460a088ae93f6fdebd63771cc674b8
William Freudenberger <w.freude@icloud.com>
2025-04-27 10:31:49 +0200

    fmt

--- src/vaults/interfaces/investments/ISharePriceProvider.sol
+++ src/vaults/interfaces/investments/ISharePriceProvider.sol
@@ -6,5 +6,3 @@ import {D18} from "src/misc/types/D18.sol";
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
 import {AssetId} from "src/common/types/AssetId.sol";
-
-
