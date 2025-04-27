7c5e127d63c05d40d650db900f98beab9aaa5e86
William Freudenberger <w.freude@icloud.com>
2025-04-27 14:20:25 +0200

    refactor: rm ISharePriceProvider (#320)
    * refactor: rm ISharePriceProvider
    
    * fmt

--- src/vaults/interfaces/investments/ISyncRequests.sol
+++ src/vaults/interfaces/investments/ISyncRequests.sol
@@ -3,14 +3,35 @@ pragma solidity 0.8.28;
 
 import {D18} from "src/misc/types/D18.sol";
 
-import {IUpdateContract} from "src/vaults/interfaces/IUpdateContract.sol";
-import {IVaultManager} from "src/vaults/interfaces/IVaultManager.sol";
-import {ISyncDepositManager} from "src/vaults/interfaces/investments/ISyncDepositManager.sol";
-import {ISharePriceProvider} from "src/vaults/interfaces/investments/ISharePriceProvider.sol";
+import {AssetId} from "src/common/types/AssetId.sol";
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
 
-interface ISyncRequests is ISyncDepositManager, ISharePriceProvider, IUpdateContract {
+import {IUpdateContract} from "src/vaults/interfaces/IUpdateContract.sol";
+import {IVaultManager} from "src/vaults/interfaces/IVaultManager.sol";
+import {ISyncDepositManager} from "src/vaults/interfaces/investments/ISyncDepositManager.sol";
+
+/// @dev Solely used locally as protection against stack-too-deep
+struct Prices {
+    /// @dev Price of 1 asset unit per share unit
+    D18 assetPerShare;
+    /// @dev Price of 1 pool unit per asset unit
+    D18 poolPerAsset;
+    /// @dev Price of 1 pool unit per share unit
+    D18 poolPerShare;
+}
+
+interface ISyncDepositValuation {
+    /// @notice Returns the pool price per share for a given pool and share class, asset, and asset id.
+    // The provided price is defined as POOL_UNIT/SHARE_UNIT.
+    ///
+    /// @param poolId The pool id
+    /// @param scId The share class id
+    /// @return price The pool price per share
+    function pricePoolPerShare(PoolId poolId, ShareClassId scId) external view returns (D18 price);
+}
+
+interface ISyncRequests is ISyncDepositManager, ISyncDepositValuation, IUpdateContract {
     event SetValuation(PoolId indexed poolId, ShareClassId indexed scId, address valuation);
     event SetMaxReserve(
         PoolId indexed poolId, ShareClassId indexed scId, address asset, uint256 tokenId, uint128 maxReserve
@@ -40,4 +61,15 @@ interface ISyncRequests is ISyncDepositManager, ISharePriceProvider, IUpdateCont
     /// @param maxReserve The amount of maximum reserve
     function setMaxReserve(PoolId poolId, ShareClassId scId, address asset, uint256 tokenId, uint128 maxReserve)
         external;
+
+    /// @notice Returns the all three prices for a given pool, share class, asset, and asset id.
+    ///
+    /// @param poolId The pool id
+    /// @param scId The share class id
+    /// @param assetId The asset id corresponding to the asset and tokenId
+    /// @return priceData The asset price per share, pool price per asset, and pool price per share
+    function prices(PoolId poolId, ShareClassId scId, AssetId assetId)
+        external
+        view
+        returns (Prices memory priceData);
 }
