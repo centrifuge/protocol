7d330a0cb433535944b092ef8ff0886c3ca8f04c
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-05-09 10:17:10 -0300

    fix: updating ghosts in handlers

--- test/integration/recon-end-to-end/targets/AdminTargets.sol
+++ test/integration/recon-end-to-end/targets/AdminTargets.sol
@@ -42,14 +42,14 @@ abstract contract AdminTargets is
     /// === STATE FUNCTIONS === ///
     /// @dev These explicitly clamp the investor to always be one of the actors
 
-    function hub_addShareClass(uint256 salt) public {
+    function hub_addShareClass(uint256 salt) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         string memory name = "Test ShareClass";
         string memory symbol = "TSC";
         hub.addShareClass(poolId, name, symbol, bytes32(salt));
     }
 
-    function hub_approveDeposits(uint32 nowDepositEpochId, uint128 maxApproval) public {
+    function hub_approveDeposits(uint32 nowDepositEpochId, uint128 maxApproval) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId paymentAssetId = hubRegistry.currency(poolId);
@@ -62,7 +62,7 @@ abstract contract AdminTargets is
         approvedDeposits += approvedAssetAmount;
     }
 
-    function hub_approveRedeems(uint32 nowRedeemEpochId, uint128 maxApproval) public {
+    function hub_approveRedeems(uint32 nowRedeemEpochId, uint128 maxApproval) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId payoutAssetId = AssetId.wrap(_getAssetId());
@@ -79,7 +79,7 @@ abstract contract AdminTargets is
         hub_approveRedeems(nowRedeemEpochId, maxApproval);
     }
 
-    function hub_createAccount(uint32 accountAsInt, bool isDebitNormal) public {
+    function hub_createAccount(uint32 accountAsInt, bool isDebitNormal) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         AccountId account = AccountId.wrap(accountAsInt);
         hub.createAccount(poolId, account, isDebitNormal);
@@ -87,7 +87,7 @@ abstract contract AdminTargets is
         createdAccountIds.push(account);
     }
 
-    function hub_createHolding(IERC7726 valuation, uint32 assetAccountAsUint, uint32 equityAccountAsUint, uint32 lossAccountAsUint, uint32 gainAccountAsUint) public {
+    function hub_createHolding(IERC7726 valuation, uint32 assetAccountAsUint, uint32 equityAccountAsUint, uint32 lossAccountAsUint, uint32 gainAccountAsUint) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = hubRegistry.currency(poolId);
@@ -114,7 +114,7 @@ abstract contract AdminTargets is
         hub_createHolding(valuation, assetAccount.raw(), equityAccount.raw(), lossAccount.raw(), gainAccount.raw());
     }
 
-    function hub_createLiability(IERC7726 valuation, uint32 expenseAccountAsUint, uint32 liabilityAccountAsUint) public {
+    function hub_createLiability(IERC7726 valuation, uint32 expenseAccountAsUint, uint32 liabilityAccountAsUint) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = AssetId.wrap(_getAssetId());
@@ -129,7 +129,7 @@ abstract contract AdminTargets is
         hub_createLiability(valuation, expenseAccount.raw(), liabilityAccount.raw());
     }
     
-    function hub_issueShares(uint32 nowIssueEpochId, uint128 navPerShare) public {
+    function hub_issueShares(uint32 nowIssueEpochId, uint128 navPerShare) public updateGhostsWithType(OpType.ISSUE) {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = AssetId.wrap(_getAssetId());
@@ -140,7 +140,7 @@ abstract contract AdminTargets is
         hub_issueShares(nowIssueEpochId, navPerShare);
     }
 
-    function hub_notifyPool(uint16 centrifugeId) public {
+    function hub_notifyPool(uint16 centrifugeId) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         hub.notifyPool(poolId, centrifugeId);
     }
@@ -149,7 +149,7 @@ abstract contract AdminTargets is
         hub_notifyPool(CENTRIFUGE_CHAIN_ID);
     }
 
-    function hub_notifyShareClass(uint16 centrifugeId, bytes32 hook) public {
+    function hub_notifyShareClass(uint16 centrifugeId, bytes32 hook) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.notifyShareClass(poolId, scId, centrifugeId, hook);
@@ -159,7 +159,7 @@ abstract contract AdminTargets is
         hub_notifyShareClass(CENTRIFUGE_CHAIN_ID, hook);
     }
 
-    function hub_notifySharePrice(uint16 centrifugeId) public {
+    function hub_notifySharePrice(uint16 centrifugeId) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.notifySharePrice(poolId, scId, centrifugeId);
@@ -169,14 +169,14 @@ abstract contract AdminTargets is
         hub_notifySharePrice(CENTRIFUGE_CHAIN_ID);
     }
     
-    function hub_notifyAssetPrice() public {
+    function hub_notifyAssetPrice() public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = hubRegistry.currency(poolId);
         hub.notifyAssetPrice(poolId, scId, assetId);
     }
 
-    function hub_triggerIssueShares(uint16 centrifugeId, uint128 shares) public {
+    function hub_triggerIssueShares(uint16 centrifugeId, uint128 shares) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.triggerIssueShares(centrifugeId, poolId, scId, _getActor(), shares);
@@ -186,7 +186,7 @@ abstract contract AdminTargets is
         hub_triggerIssueShares(CENTRIFUGE_CHAIN_ID, shares);
     }
 
-    function hub_triggerSubmitQueuedShares(uint16 centrifugeId) public {
+    function hub_triggerSubmitQueuedShares(uint16 centrifugeId) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.triggerSubmitQueuedShares(centrifugeId, poolId, scId);
@@ -196,7 +196,7 @@ abstract contract AdminTargets is
         hub_triggerSubmitQueuedShares(CENTRIFUGE_CHAIN_ID);
     }
 
-    function hub_triggerSubmitQueuedAssets(uint128 assetIdAsUint) public {
+    function hub_triggerSubmitQueuedAssets(uint128 assetIdAsUint) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = AssetId.wrap(_getAssetId());
@@ -209,7 +209,7 @@ abstract contract AdminTargets is
         hub_triggerSubmitQueuedAssets(assetId.raw());
     }
     
-    function hub_setQueue(uint16 centrifugeId, bool enabled) public {
+    function hub_setQueue(uint16 centrifugeId, bool enabled) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.setQueue(centrifugeId, poolId, scId, enabled);
@@ -219,20 +219,20 @@ abstract contract AdminTargets is
         hub_setQueue(CENTRIFUGE_CHAIN_ID, enabled);
     }
 
-    function hub_revokeShares(uint32 nowRevokeEpochId, uint128 navPerShare) public {
+    function hub_revokeShares(uint32 nowRevokeEpochId, uint128 navPerShare) public updateGhostsWithType(OpType.REVOKE) {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId payoutAssetId = hubRegistry.currency(poolId);
         hub.revokeShares(poolId, scId, payoutAssetId, nowRevokeEpochId, D18.wrap(navPerShare));
     }
 
-    function hub_setAccountMetadata(uint32 accountAsInt, bytes memory metadata) public {
+    function hub_setAccountMetadata(uint32 accountAsInt, bytes memory metadata) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         AccountId account = AccountId.wrap(accountAsInt);
         hub.setAccountMetadata(poolId, account, metadata);
     }
 
-    function hub_setHoldingAccountId(uint128 assetIdAsUint, uint8 kind, uint32 accountIdAsInt) public {
+    function hub_setHoldingAccountId(uint128 assetIdAsUint, uint8 kind, uint32 accountIdAsInt) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = AssetId.wrap(assetIdAsUint);
@@ -240,12 +240,12 @@ abstract contract AdminTargets is
         hub.setHoldingAccountId(poolId, scId, assetId, kind, accountId);
     }
 
-    function hub_setPoolMetadata(bytes memory metadata) public {
+    function hub_setPoolMetadata(bytes memory metadata) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         hub.setPoolMetadata(poolId, metadata);
     }
 
-    function hub_updateHoldingValuation(uint128 assetIdAsUint, IERC7726 valuation) public {
+    function hub_updateHoldingValuation(uint128 assetIdAsUint, IERC7726 valuation) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         AssetId assetId = AssetId.wrap(assetIdAsUint);
@@ -259,7 +259,7 @@ abstract contract AdminTargets is
         hub_updateHoldingValuation(assetId.raw(), valuation);
     }
 
-    function hub_updateRestriction(uint16 chainId, bytes calldata payload) public {
+    function hub_updateRestriction(uint16 chainId, bytes calldata payload) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.updateRestriction(poolId, scId, chainId, payload);
@@ -269,13 +269,13 @@ abstract contract AdminTargets is
         hub_updateRestriction(CENTRIFUGE_CHAIN_ID, payload);
     }
 
-    function hub_updatePricePerShare(uint128 navPerShare) public {
+    function hub_updatePricePerShare(uint128 navPerShare) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         hub.updatePricePerShare(poolId, scId, D18.wrap(navPerShare));
     }
 
-    function syncRequestManager_setValuation(address valuation) public {
+    function syncRequestManager_setValuation(address valuation) public updateGhosts {
         PoolId poolId = PoolId.wrap(_getPool());
         ShareClassId scId = ShareClassId.wrap(_getShareClassId());
         syncRequestManager.setValuation(poolId, scId, valuation);
@@ -285,58 +285,5 @@ abstract contract AdminTargets is
         address valuation = isIdentityValuation ? address(identityValuation) : address(transientValuation);
         syncRequestManager_setValuation(valuation);
     }
-    
     /// AUTO GENERATED TARGET FUNCTIONS - WARNING: DO NOT DELETE OR MODIFY THIS LINE ///
-
-    // === PoolManager === //
-    /// Gateway owner methods: these get called directly because we're not using the gateway in our setup
-    /// @notice These don't prank asAdmin because there are external calls first, 
-    /// @notice admin is the tester contract (address(this)) so we leave out an explicit prank directly before the call to the target function
-    /// NOTE: commented functions don't need to have handlers anymore because they're called as callbacks to operations in the Vault 
-
-    // function hub_registerAsset(uint128 assetIdAsUint) public updateGhosts {
-    //     AssetId assetId_ = AssetId.wrap(assetIdAsUint); 
-    //     uint8 decimals = MockERC20(_getAsset()).decimals();
-
-    //     hub.registerAsset(assetId_, decimals);
-
-    //     createdAssetIds.push(assetId_);
-    // }  
-
-    // function hub_registerAsset_clamped() public updateGhosts {
-    //     uint128 assetId = assetAddressToAssetId[_getAsset()];
-    //     hub_registerAsset(assetId);
-    // }
-
-    /// @dev Property: after successfully calling requestDeposit for an investor, their depositRequest[..].lastUpdate equals the current epoch id epochId[poolId]
-    /// @dev Property: _updateDepositRequest should never revert due to underflow
-    /// @dev Property: The total pending deposit amount pendingDeposit[..] is always >= the sum of pending user deposit amounts depositRequest[..]
-    // function hub_depositRequest(uint64 poolIdAsUint, bytes16 scIdAsBytes, uint128 amount) public updateGhosts {
-    //     PoolId poolId = PoolId.wrap(poolIdAsUint);
-    //     ShareClassId scId = ShareClassId.wrap(scIdAsBytes);
-    //     AssetId depositAssetId = hubRegistry.currency(poolId);
-
-    // function hub_increaseShareIssuance(uint64 poolIdAsUint, bytes16 scIdAsBytes, uint128 amount) public updateGhosts {
-    //     PoolId poolId = PoolId.wrap(poolIdAsUint);
-    //     ShareClassId scId = ShareClassId.wrap(scIdAsBytes);
-    //     hub.increaseShareIssuance(poolId, scId, amount);
-    // }
-
-    // function hub_increaseShareIssuance_clamped(uint64 poolEntropy, uint32 scEntropy, uint128 pricePerShare, uint128 amount) public updateGhosts {
-    //     PoolId poolId = Helpers.getRandomPoolId(_getPools(), poolEntropy);
-    //     ShareClassId scId = Helpers.getRandomShareClassIdForPool(shareClassManager, poolId, scEntropy);
-    //     hub_increaseShareIssuance(poolId.raw(), scId.raw(), amount);
-    // }
-
-    // function hub_decreaseShareIssuance(uint64 poolIdAsUint, bytes16 scIdAsBytes, uint128 amount) public updateGhosts {
-    //     PoolId poolId = PoolId.wrap(poolIdAsUint);
-    //     ShareClassId scId = ShareClassId.wrap(scIdAsBytes);
-    //     hub.decreaseShareIssuance(poolId, scId, amount);
-    // }
-
-    // function hub_decreaseShareIssuance_clamped(uint64 poolEntropy, uint32 scEntropy, uint128 amount) public updateGhosts {
-    //     PoolId poolId = Helpers.getRandomPoolId(_getPools(), poolEntropy);
-    //     ShareClassId scId = Helpers.getRandomShareClassIdForPool(shareClassManager, poolId, scEntropy);
-    //     hub_decreaseShareIssuance(poolId.raw(), scId.raw(), amount);
-    // }
 }
