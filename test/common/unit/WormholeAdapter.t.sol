ebae20248655a60f32b4a8d87f40be7f34b7647c
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2025-05-20 14:38:09 +0200

    MultiAdapter (#372)
    * base implementation
    
    * apply comments
    
    * fix tests
    
    * fix tests
    
    * renames
    
    * minor rename
    
    * fix
    
    * rename to entrypoint and missing doc refs
    
    * adapters interfaces in its own folder
    
    * minor rename
    
    * apply comments
    
    * Helper organization
    
    * fix legacy test failing
    
    * factorize addUnpaidMessage

--- test/common/unit/WormholeAdapter.t.sol
+++ test/common/unit/WormholeAdapter.t.sol
@@ -9,7 +9,7 @@ import {IAuth} from "src/misc/interfaces/IAuth.sol";
 import {Mock} from "test/common/mocks/Mock.sol";
 
 import {WormholeAdapter} from "src/common/adapters/WormholeAdapter.sol";
-import {IWormholeAdapter} from "src/common/interfaces/IWormholeAdapter.sol";
+import {IWormholeAdapter} from "src/common/interfaces/adapters/IWormholeAdapter.sol";
 import {IMessageHandler} from "src/common/interfaces/IMessageHandler.sol";
 import {IAdapter} from "src/common/interfaces/IAdapter.sol";
 
@@ -67,7 +67,7 @@ contract WormholeAdapterTest is Test {
     }
 
     function testDeploy() public view {
-        assertEq(address(adapter.gateway()), address(GATEWAY));
+        assertEq(address(adapter.entrypoint()), address(GATEWAY));
         assertEq(address(adapter.relayer()), address(relayer));
         assertEq(adapter.localWormholeId(), 2);
 
@@ -152,7 +152,7 @@ contract WormholeAdapterTest is Test {
         vm.assume(invalidOrigin != address(GATEWAY));
 
         vm.deal(address(this), 0.1 ether);
-        vm.expectRevert(IAdapter.NotGateway.selector);
+        vm.expectRevert(IAdapter.NotEntrypoint.selector);
         adapter.send{value: 0.1 ether}(CENTRIFUGE_CHAIN_ID, payload, gasLimit, refund);
 
         vm.deal(address(GATEWAY), 0.1 ether);
