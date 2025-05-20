ce65b31481af49a1d95560e06f9bce9fd4b735af
lemunozm <lemunozm@gmail.com>
2025-05-20 14:40:20 +0200

    paths spokes to spoke

---
+++ test/spoke/integration/Burn.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import "test/spoke/BaseTest.sol";
+import {IAuth} from "src/misc/interfaces/IAuth.sol";
+import {IERC20} from "src/misc/interfaces/IERC20.sol";
+
+contract BurnTest is BaseTest {
+    function testBurn(uint256 amount) public {
+        amount = uint128(bound(amount, 2, MAX_UINT128));
+
+        (, address vault_,) = deploySimpleVault(VaultKind.Async);
+        AsyncVault vault = AsyncVault(vault_);
+
+        IShareToken shareToken = IShareToken(address(vault.share()));
+        root.relyContract(address(shareToken), self); // give self auth permissions
+        // add investor as member
+        centrifugeChain.updateMember(vault.poolId().raw(), vault.scId().raw(), investor, type(uint64).max);
+
+        shareToken.mint(investor, amount);
+        root.denyContract(address(shareToken), self); // remove auth permissions from self
+
+        vm.expectRevert(IAuth.NotAuthorized.selector);
+        shareToken.burn(investor, amount);
+
+        root.relyContract(address(shareToken), self); // give self auth permissions
+        vm.expectRevert(IERC20.InsufficientAllowance.selector);
+        shareToken.burn(investor, amount);
+
+        // success
+        vm.prank(investor);
+        shareToken.approve(self, amount); // approve to burn shareTokens
+        shareToken.burn(investor, amount);
+
+        assertEq(shareToken.balanceOf(investor), 0);
+        assertEq(shareToken.balanceOf(investor), shareToken.balanceOf(investor));
+    }
+}
---
+++ test/spoke/unit/RestrictedTransfers.t.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.28;
+
+import "forge-std/Test.sol";
+
+import {MockRoot} from "test/common/mocks/MockRoot.sol";
+
+import {IERC165} from "src/misc/interfaces/IERC7575.sol";
+
+import {ShareToken} from "src/spoke/ShareToken.sol";
+import {IHook} from "src/common/interfaces/IHook.sol";
+
+import {FullRestrictions} from "src/hooks/FullRestrictions.sol";
+import {IMemberlist} from "src/hooks/interfaces/IMemberlist.sol";
+import {IFreezable} from "src/hooks/interfaces/IFreezable.sol";
+
+contract FullRestrictionsTest is Test {
+    MockRoot root;
+    ShareToken token;
+    FullRestrictions fullRestrictionsHook;
+
+    function setUp() public {
+        root = new MockRoot();
+        token = new ShareToken(18);
+        fullRestrictionsHook = new FullRestrictions(address(root), address(this));
+        token.file("hook", address(fullRestrictionsHook));
+    }
+
+    function testHandleInvalidMessage() public {
+        vm.expectRevert(IHook.InvalidUpdate.selector);
+        fullRestrictionsHook.updateRestriction(address(token), abi.encodePacked(uint8(0)));
+    }
+
+    function testAddMember(uint64 validUntil) public {
+        vm.assume(validUntil >= block.timestamp);
+
+        vm.expectRevert(IMemberlist.InvalidValidUntil.selector);
+        fullRestrictionsHook.updateMember(address(token), address(this), uint64(block.timestamp - 1));
+
+        fullRestrictionsHook.updateMember(address(token), address(this), validUntil);
+        (bool _isMember, uint64 _validUntil) = fullRestrictionsHook.isMember(address(token), address(this));
+        assertTrue(_isMember);
+        assertEq(_validUntil, validUntil);
+    }
+
+    function testIsMember(uint64 validUntil) public {
+        vm.assume(validUntil >= block.timestamp);
+
+        fullRestrictionsHook.updateMember(address(token), address(this), validUntil);
+        (bool _isMember, uint64 _validUntil) = fullRestrictionsHook.isMember(address(token), address(this));
+        assertTrue(_isMember);
+        assertEq(_validUntil, validUntil);
+    }
+
+    function testFreeze() public {
+        fullRestrictionsHook.freeze(address(token), address(this));
+        assertEq(fullRestrictionsHook.isFrozen(address(token), address(this)), true);
+    }
+
+    function testFreezingZeroAddress() public {
+        vm.expectRevert(IFreezable.CannotFreezeZeroAddress.selector);
+        fullRestrictionsHook.freeze(address(token), address(0));
+        assertEq(fullRestrictionsHook.isFrozen(address(token), address(0)), false);
+    }
+
+    function testAddMemberAndFreeze(uint64 validUntil) public {
+        vm.assume(validUntil >= block.timestamp);
+
+        fullRestrictionsHook.updateMember(address(token), address(this), validUntil);
+        (bool _isMember, uint64 _validUntil) = fullRestrictionsHook.isMember(address(token), address(this));
+        assertTrue(_isMember);
+        assertEq(_validUntil, validUntil);
+        assertEq(fullRestrictionsHook.isFrozen(address(token), address(this)), false);
+
+        fullRestrictionsHook.freeze(address(token), address(this));
+        (_isMember, _validUntil) = fullRestrictionsHook.isMember(address(token), address(this));
+        assertTrue(_isMember);
+        assertEq(_validUntil, validUntil);
+        assertEq(fullRestrictionsHook.isFrozen(address(token), address(this)), true);
+    }
+
+    // --- erc165 checks ---
+    function testERC165Support(bytes4 unsupportedInterfaceId) public view {
+        bytes4 erc165 = 0x01ffc9a7;
+        bytes4 hook = 0xad4e9d84;
+
+        vm.assume(unsupportedInterfaceId != erc165 && unsupportedInterfaceId != hook);
+
+        assertEq(type(IERC165).interfaceId, erc165);
+        assertEq(type(IHook).interfaceId, hook);
+
+        assertEq(fullRestrictionsHook.supportsInterface(erc165), true);
+        assertEq(fullRestrictionsHook.supportsInterface(hook), true);
+
+        assertEq(fullRestrictionsHook.supportsInterface(unsupportedInterfaceId), false);
+    }
+}
