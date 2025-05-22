902a4e1bbcb6f830924656de7b5cc60a3ac3e7f2
Jeroen Offerijns <git@o6ns.com>
2025-05-22 14:40:16 +0200

    Fix warnings

--- src/managers/decoders/CircleDecoder.sol
+++ src/managers/decoders/CircleDecoder.sol
@@ -8,20 +8,16 @@ contract CircleDecoder is BaseDecoder {
     // @tag destinationDomain:uint32:the id of the destination chain
     // @tag mintRecipient:bytes32:the address of the recipient on the destination chain in a bytes32 format
     // @tag burnToken:address:the address of the token to burn
-    function depositForBurn(uint256 amount, uint32 destinationDomain, bytes32 mintRecipient, address burnToken)
+    function depositForBurn(uint256, uint32 destinationDomain, bytes32 mintRecipient, address burnToken)
         external
-        view
+        pure
         returns (bytes memory addressesFound)
     {
         addressesFound = abi.encodePacked(destinationDomain, mintRecipient, burnToken);
     }
 
     // @desc Receive a message from Circle, in order to mint tokens on the destination chain
-    function receiveMessage(bytes memory message, bytes memory attestation)
-        external
-        view
-        returns (bytes memory addressesFound)
-    {
+    function receiveMessage(bytes memory, bytes memory) external pure returns (bytes memory addressesFound) {
         // nothing to sanitize
         return addressesFound;
     }
