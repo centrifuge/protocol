9e7bf5ae36dbdbb93205fc1e5ddf2fbd7e0f92e4
Jeroen Offerijns <git@o6ns.com>
2025-05-26 13:58:23 +0200

    Typos

--- src/common/interfaces/ISnapshotHook.sol
+++ src/common/interfaces/ISnapshotHook.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.28;
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
 
-/// @notice Hook interface that is called whenever the accounting system of the Hub, for a given share token
-///         1 network, aka a (poolId, scId, centrifugeId) tuple, is in a synchronous state. This means the assets
+/// @notice Hook interface that is called whenever the accounting system of the Hub, for a given share token on
+///         1 network, as in a (poolId, scId, centrifugeId) tuple, is in a synchronous state. This means the assets
 ///         (deposits/withdrawals of holdings) are updated in aligment with the issuance of shares.
 ///
 ///         This can be used to compute higher-level properties such as the NAV and NAV/share, which require
 ///         assets and shares to be in sync to be accurate.
 ///
 ///         NOTE: Only the state for the given centrifugeId is guaranteed to be in sync. This means, if
-///         the snapshot hook reads state from the accounting latyer, the accounts of holdings and liabilities
+///         the snapshot hook reads state from the accounting layer, the accounts of holdings and liabilities
 ///         should be set up per network, to not mingle in sync and out of sync state.
 interface ISnapshotHook {
     /// @notice Callback on snapshot.
