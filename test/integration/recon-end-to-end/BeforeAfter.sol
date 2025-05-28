43a2ae265129ba420930e29f5b8ea50c4b4a2412
nelson-pereira8 <94120714+nican0r@users.noreply.github.com>
2025-05-28 19:18:25 -0300

    fix: get rid of loops in BeforeAfter

--- test/integration/recon-end-to-end/BeforeAfter.sol
+++ test/integration/recon-end-to-end/BeforeAfter.sol
@@ -99,37 +99,32 @@ abstract contract BeforeAfter is Setup {
         // Hub
         _before.ghostDebited = accounting.debited();
         _before.ghostCredited = accounting.credited();
-        uint64[] memory _createdPools = _getPools();
-        for (uint256 i = 0; i < _createdPools.length; i++) {
-            address[] memory _actors = _getActors();
-            PoolId poolId = PoolId.wrap(_createdPools[i]);
-            
-            // loop through all share classes for the pool
-            for (uint32 j = 0; j < shareClassManager.shareClassCount(poolId); j++) {
-                ShareClassId scId = shareClassManager.previewShareClassId(poolId, j);
-                AssetId assetId = hubRegistry.currency(poolId);
 
-                (uint32 depositEpochId, uint32 redeemEpochId, uint32 issueEpochId, uint32 revokeEpochId) = shareClassManager.epochId(scId, assetId);
-                _before.ghostEpochId[scId][assetId] = EpochId({deposit: depositEpochId, redeem: redeemEpochId, issue: issueEpochId, revoke: revokeEpochId});
-                (, _before.ghostHolding[poolId][scId][assetId],,) = holdings.holding(poolId, scId, assetId);
-                
-                // loop over all actors
-                for (uint256 k = 0; k < _actors.length; k++) {
-                    bytes32 actor = CastLib.toBytes32(_actors[k]);
-                    (uint128 pendingRedeem, uint32 lastUpdate) = shareClassManager.redeemRequest(scId, assetId, actor);
-                    _before.ghostRedeemRequest[scId][assetId][actor] = UserOrder({pending: pendingRedeem, lastUpdate: lastUpdate});
-                }
+        IBaseVault vault = IBaseVault(_getVault());
+        PoolId poolId = vault.poolId();
+        ShareClassId scId = vault.scId();
+        AssetId assetId = hubRegistry.currency(poolId);
 
-                // loop over all account types defined in IHub::AccountType
-                for(uint8 kind = 0; kind < 6; kind++) {
-                    AccountId accountId = holdings.accountId(poolId, scId, assetId, kind);
-                    (,,,uint64 lastUpdated,) = accounting.accounts(poolId, accountId);
-                    // accountValue is only set if the account has been updated
-                    if(lastUpdated != 0) {
-                        (bool isPositive, uint128 accountValue) = accounting.accountValue(poolId, accountId);
-                        _before.ghostAccountValue[poolId][accountId] = accountValue;
-                    }
-                }
+        (uint32 depositEpochId, uint32 redeemEpochId, uint32 issueEpochId, uint32 revokeEpochId) = shareClassManager.epochId(scId, assetId);
+        _before.ghostEpochId[scId][assetId] = EpochId({deposit: depositEpochId, redeem: redeemEpochId, issue: issueEpochId, revoke: revokeEpochId});
+        (, _before.ghostHolding[poolId][scId][assetId],,) = holdings.holding(poolId, scId, assetId);
+        
+        // loop over all actors
+        address[] memory _actors = _getActors();
+        for (uint256 k = 0; k < _actors.length; k++) {
+            bytes32 actor = CastLib.toBytes32(_actors[k]);
+            (uint128 pendingRedeem, uint32 lastUpdate) = shareClassManager.redeemRequest(scId, assetId, actor);
+            _before.ghostRedeemRequest[scId][assetId][actor] = UserOrder({pending: pendingRedeem, lastUpdate: lastUpdate});
+        }
+
+        // loop over all account types defined in IHub::AccountType
+        for(uint8 kind = 0; kind < 6; kind++) {
+            AccountId accountId = holdings.accountId(poolId, scId, assetId, kind);
+            (,,,uint64 lastUpdated,) = accounting.accounts(poolId, accountId);
+            // accountValue is only set if the account has been updated
+            if(lastUpdated != 0) {
+                (bool isPositive, uint128 accountValue) = accounting.accountValue(poolId, accountId);
+                _before.ghostAccountValue[poolId][accountId] = accountValue;
             }
         }
     }
@@ -146,37 +141,32 @@ abstract contract BeforeAfter is Setup {
         // Hub
         _after.ghostDebited = accounting.debited();
         _after.ghostCredited = accounting.credited();
-        uint64[] memory _createdPools = _getPools();
-        for (uint256 i = 0; i < _createdPools.length; i++) {
-            address[] memory _actors = _getActors();
-            PoolId poolId = PoolId.wrap(_createdPools[i]);
-            
-            // loop through all share classes for the pool
-            for (uint32 j = 0; j < shareClassManager.shareClassCount(poolId); j++) {
-                ShareClassId scId = shareClassManager.previewShareClassId(poolId, j);
-                AssetId assetId = hubRegistry.currency(poolId);
+        
+        IBaseVault vault = IBaseVault(_getVault());
+        PoolId poolId = vault.poolId();
+        ShareClassId scId = vault.scId();
+        AssetId assetId = hubRegistry.currency(poolId);
 
-                (uint32 depositEpochId, uint32 redeemEpochId, uint32 issueEpochId, uint32 revokeEpochId) = shareClassManager.epochId(scId, assetId);
-                _after.ghostEpochId[scId][assetId] = EpochId({deposit: depositEpochId, redeem: redeemEpochId, issue: issueEpochId, revoke: revokeEpochId});
-                (, _after.ghostHolding[poolId][scId][assetId],,) = holdings.holding(poolId, scId, assetId);
-                
-                // loop over all actors
-                for (uint256 k = 0; k < _actors.length; k++) {
-                    bytes32 actor = CastLib.toBytes32(_actors[k]);
-                    (uint128 pendingRedeem, uint32 lastUpdate) = shareClassManager.redeemRequest(scId, assetId, actor);
-                    _after.ghostRedeemRequest[scId][assetId][actor] = UserOrder({pending: pendingRedeem, lastUpdate: lastUpdate});
-                }
+        (uint32 depositEpochId, uint32 redeemEpochId, uint32 issueEpochId, uint32 revokeEpochId) = shareClassManager.epochId(scId, assetId);
+        _after.ghostEpochId[scId][assetId] = EpochId({deposit: depositEpochId, redeem: redeemEpochId, issue: issueEpochId, revoke: revokeEpochId});
+        (, _after.ghostHolding[poolId][scId][assetId],,) = holdings.holding(poolId, scId, assetId);
+        
+        // loop over all actors
+        address[] memory _actors = _getActors();
+        for (uint256 k = 0; k < _actors.length; k++) {
+            bytes32 actor = CastLib.toBytes32(_actors[k]);
+            (uint128 pendingRedeem, uint32 lastUpdate) = shareClassManager.redeemRequest(scId, assetId, actor);
+            _after.ghostRedeemRequest[scId][assetId][actor] = UserOrder({pending: pendingRedeem, lastUpdate: lastUpdate});
+        }
 
-                // loop over all account types defined in IHub::AccountType
-                for(uint8 kind = 0; kind < 6; kind++) {
-                    AccountId accountId = holdings.accountId(poolId, scId, assetId, kind);
-                    (,,,uint64 lastUpdated,) = accounting.accounts(poolId, accountId);
-                    // accountValue is only set if the account has been updated
-                    if(lastUpdated != 0) {
-                        (bool isPositive, uint128 accountValue) = accounting.accountValue(poolId, accountId);
-                        _after.ghostAccountValue[poolId][accountId] = accountValue;
-                    }
-                }
+        // loop over all account types defined in IHub::AccountType
+        for(uint8 kind = 0; kind < 6; kind++) {
+            AccountId accountId = holdings.accountId(poolId, scId, assetId, kind);
+            (,,,uint64 lastUpdated,) = accounting.accounts(poolId, accountId);
+            // accountValue is only set if the account has been updated
+            if(lastUpdated != 0) {
+                (bool isPositive, uint128 accountValue) = accounting.accountValue(poolId, accountId);
+                _after.ghostAccountValue[poolId][accountId] = accountValue;
             }
         }
     }
