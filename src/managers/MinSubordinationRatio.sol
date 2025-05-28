3533df4440aaf61e5050f3eac433c2d04e4716be
Jeroen Offerijns <git@o6ns.com>
2025-05-28 08:40:51 +0200

    Update for latest Hub interface

--- src/managers/MinSubordinationRatio.sol
+++ src/managers/MinSubordinationRatio.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.28;
 
 import {Auth} from "src/misc/Auth.sol";
-import {D18, d18, divD8} from "src/misc/types/D18.sol";
-import {IERC7726} from "src/misc/interfaces/IERC7726.sol";
+import {D18, d18} from "src/misc/types/D18.sol";
 
+import {IValuation} from "src/common/interfaces/IValuation.sol";
 import {PoolId} from "src/common/types/PoolId.sol";
 import {AssetId} from "src/common/types/AssetId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
@@ -18,7 +18,7 @@ contract MinSubordination is Auth {
 
     IHub public immutable hub;
     IHoldings public immutable holdings;
-    IShareClassManager public immutable shareClassManager;
+    IShareClassManager public immutable scm;
 
     PoolId public immutable poolId;
     ShareClassId public immutable seniorScId;
@@ -29,14 +29,14 @@ contract MinSubordination is Auth {
     constructor(
         IHub hub_,
         IHoldings holdings_,
-        IShareClassManager shareClassManager_,
+        IShareClassManager scm_,
         PoolId poolId_,
         ShareClassId seniorScId_,
         ShareClassId juniorScId_
     ) Auth(msg.sender) {
         hub = hub_;
         holdings = holdings_;
-        shareClassManager = shareClassManager_;
+        scm = scm_;
 
         poolId = poolId_;
         seniorScId = seniorScId_;
@@ -59,30 +59,30 @@ contract MinSubordination is Auth {
         uint128 juniorRedeem,
         D18 juniorNavPerShare
     ) external auth {
-        IERC7726 valuation = holdings.valuation(poolId, seniorScId, assetId);
+        IValuation valuation = holdings.valuation(poolId, seniorScId, assetId);
 
-        hub.updatePricePoolPerShare(poolId, seniorScId, seniorNavPerShare, bytes(""));
-        hub.updatePricePoolPerShare(poolId, juniorScId, juniorNavPerShare, bytes(""));
+        hub.updateSharePrice(poolId, seniorScId, seniorNavPerShare);
+        hub.updateSharePrice(poolId, juniorScId, juniorNavPerShare);
 
-        hub.approveDeposits(poolId, seniorScId, assetId, seniorDeposit, valuation);
-        hub.issueShares(poolId, seniorScId, assetId, seniorNavPerShare);
+        hub.approveDeposits(poolId, seniorScId, assetId, scm.nowDepositEpoch(seniorScId, assetId), seniorDeposit);
+        hub.issueShares(poolId, seniorScId, assetId, scm.nowIssueEpoch(seniorScId, assetId), seniorNavPerShare);
 
-        hub.approveRedeems(poolId, seniorScId, assetId, seniorRedeem);
-        hub.revokeShares(poolId, seniorScId, assetId, seniorNavPerShare, valuation);
+        hub.approveRedeems(poolId, seniorScId, assetId, scm.nowRedeemEpoch(seniorScId, assetId), seniorRedeem);
+        hub.revokeShares(poolId, seniorScId, assetId, scm.nowRevokeEpoch(seniorScId, assetId), seniorNavPerShare);
 
-        hub.approveDeposits(poolId, juniorScId, assetId, juniorDeposit, valuation);
-        hub.issueShares(poolId, juniorScId, assetId, juniorNavPerShare);
+        hub.approveDeposits(poolId, juniorScId, assetId, scm.nowDepositEpoch(juniorScId, assetId), juniorDeposit);
+        hub.issueShares(poolId, juniorScId, assetId, scm.nowIssueEpoch(juniorScId, assetId), juniorNavPerShare);
 
-        hub.approveRedeems(poolId, juniorScId, assetId, juniorRedeem);
-        hub.revokeShares(poolId, juniorScId, assetId, juniorNavPerShare, valuation);
+        hub.approveRedeems(poolId, juniorScId, assetId, scm.nowRedeemEpoch(juniorScId, assetId), juniorRedeem);
+        hub.revokeShares(poolId, juniorScId, assetId, scm.nowRevokeEpoch(juniorScId, assetId), juniorNavPerShare);
 
         _checkRatio();
     }
 
     // --- Validation ---
     function _checkRatio() internal view {
-        (uint128 seniorIssuance, D18 seniorPrice) = shareClassManager.shareClassPrice(poolId, seniorScId);
-        (uint128 juniorIssuance, D18 juniorPrice) = shareClassManager.shareClassPrice(poolId, juniorScId);
+        (uint128 seniorIssuance, D18 seniorPrice) = scm.metrics(seniorScId);
+        (uint128 juniorIssuance, D18 juniorPrice) = scm.metrics(juniorScId);
 
         D18 seniorValue = d18(seniorIssuance) * seniorPrice;
         D18 juniorValue = d18(juniorIssuance) * juniorPrice;
