4d1e17388bb2bfffddf1425911bec17500843876
William Freudenberger <w.freude@icloud.com>
2025-06-02 15:26:44 +0200

    fix: common deployer wire

--- script/CommonDeployer.s.sol
+++ script/CommonDeployer.s.sol
@@ -26,8 +26,6 @@ abstract contract CommonDeployer is Script, JsonRegistry {
     uint128 constant FALLBACK_MSG_COST = uint128(0.02 ether); // in Weight
     uint128 constant FALLBACK_MAX_BATCH_SIZE = uint128(10_000_000 ether); // 10M in Weight
 
-    IAdapter[] adapters;
-
     ISafe public adminSafe;
     Root public root;
     TokenRecoverer public tokenRecoverer;
@@ -113,18 +111,11 @@ abstract contract CommonDeployer is Script, JsonRegistry {
     }
 
     function wire(uint16 centrifugeId, IAdapter adapter, address deployer) public {
-        bool isDuplicate = false;
-        for (uint256 i = 0; i < adapters.length; i++) {
-            if (address(adapters[i]) == address(adapter)) {
-                isDuplicate = true;
-                break;
-            }
-        }
-        if (!isDuplicate) {
-            adapters.push(adapter);
-            IAuth(address(adapter)).rely(address(root));
-            IAuth(address(adapter)).deny(deployer);
-        }
+        IAuth(address(adapter)).rely(address(root));
+        IAuth(address(adapter)).deny(deployer);
+
+        IAdapter[] memory adapters = new IAdapter[](1);
+        adapters[0] = adapter;
 
         multiAdapter.file("adapters", centrifugeId, adapters);
     }
