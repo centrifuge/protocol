a816492b3ac301179df4837c815aa4c1a82b18a1
William Freudenberger <w.freude@icloud.com>
2025-06-02 15:27:02 +0200

    tests: add 3 chain configure pool

--- test/integration/ThreeChainEndToEnd.t.sol
+++ test/integration/ThreeChainEndToEnd.t.sol
@@ -32,11 +32,8 @@ contract ThreeChainEndToEndDeployment is EndToEndDeployment {
         adapterC = _deployChain(deployC, CENTRIFUGE_ID_C, CENTRIFUGE_ID_A, safeAdminC);
         vm.label(address(adapterC), "AdapterC");
 
-        adapterAToC = new LocalAdapter(
-            CENTRIFUGE_ID_A,
-            deployA.multiAdapter(),
-            address(deployA)
-        );
+        adapterAToC = new LocalAdapter(CENTRIFUGE_ID_A, deployA.multiAdapter(), address(deployA));
+        vm.label(address(adapterAToC), "AdapterAToC");
 
         // Connect Chain A to Chain C (spoke 2)
         vm.startPrank(address(deployA.root()));
@@ -87,5 +84,59 @@ contract ThreeChainEndToEndUseCases is ThreeChainEndToEndDeployment {
         sC.spoke.registerAsset{value: GAS}(h.centrifugeId, address(sC.usdc), 0);
 
         assertEq(h.hubRegistry.decimals(newAssetId(CENTRIFUGE_ID_C, 1)), 6, "expected decimals");
+    }
+
+    /// @notice Configure a pool with support for all three chains
+    /// forge-config: default.isolate = true
+    function testConfigurePoolWithThreeChains() public {
+        // Configure spokes
+        s.usdc.mint(INVESTOR_A, INVESTOR_A_USDC_AMOUNT);
+        s.spoke.registerAsset{value: GAS}(h.centrifugeId, address(s.usdc), 0);
+        sC.usdc.mint(INVESTOR_A, INVESTOR_A_USDC_AMOUNT);
+        sC.spoke.registerAsset{value: GAS}(h.centrifugeId, address(sC.usdc), 0);
+
+        // Create and configure the pool on hub (chain A)
+        vm.prank(address(h.guardian.safe()));
+        h.guardian.createPool(POOL_A, FM, USD_ID);
+
+        vm.startPrank(FM);
+        h.hub.setPoolMetadata(POOL_A, bytes("Testing pool with three chains"));
+        h.hub.addShareClass(POOL_A, "Tokenized MMF", "MMF", bytes32("salt"));
+
+        h.hub.notifyPool{value: GAS}(POOL_A, s.centrifugeId);
+        h.hub.notifyPool{value: GAS}(POOL_A, sC.centrifugeId);
+
+        h.hub.notifyShareClass{value: GAS}(POOL_A, SC_1, s.centrifugeId, s.redemptionRestrictionsHook.toBytes32());
+        h.hub.notifyShareClass{value: GAS}(POOL_A, SC_1, sC.centrifugeId, sC.redemptionRestrictionsHook.toBytes32());
+
+        h.hub.createAccount(POOL_A, ASSET_ACCOUNT, true);
+        h.hub.createAccount(POOL_A, EQUITY_ACCOUNT, false);
+        h.hub.createAccount(POOL_A, LOSS_ACCOUNT, false);
+        h.hub.createAccount(POOL_A, GAIN_ACCOUNT, false);
+
+        AssetId USDC_ID_B = newAssetId(s.centrifugeId, 1);
+        AssetId USDC_ID_C = newAssetId(sC.centrifugeId, 1);
+
+        h.hub.initializeHolding(
+            POOL_A, SC_1, USDC_ID_B, h.identityValuation, ASSET_ACCOUNT, EQUITY_ACCOUNT, GAIN_ACCOUNT, LOSS_ACCOUNT
+        );
+        h.hub.initializeHolding(
+            POOL_A, SC_1, USDC_ID_C, h.identityValuation, ASSET_ACCOUNT, EQUITY_ACCOUNT, GAIN_ACCOUNT, LOSS_ACCOUNT
+        );
+
+        h.hub.updateBalanceSheetManager{value: GAS}(s.centrifugeId, POOL_A, BSM.toBytes32(), true);
+        h.hub.updateBalanceSheetManager{value: GAS}(sC.centrifugeId, POOL_A, BSM.toBytes32(), true);
+
+        h.hub.updateSharePrice(POOL_A, SC_1, IDENTITY_PRICE);
+        h.hub.notifySharePrice{value: GAS}(POOL_A, SC_1, s.centrifugeId);
+        h.hub.notifySharePrice{value: GAS}(POOL_A, SC_1, sC.centrifugeId);
+
+        h.hub.notifyAssetPrice{value: GAS}(POOL_A, SC_1, USDC_ID_B);
+        h.hub.notifyAssetPrice{value: GAS}(POOL_A, SC_1, USDC_ID_C);
+        vm.stopPrank();
+
+        vm.startPrank(BSM);
+        s.gateway.subsidizePool{value: DEFAULT_SUBSIDY}(POOL_A);
+        sC.gateway.subsidizePool{value: DEFAULT_SUBSIDY}(POOL_A);
     }
 }
