d9ac387d340953584ca2bdb0181bddc9ded507cf
William Freudenberger <w.freude@icloud.com>
2025-06-19 12:58:55 +0200

    ref: reuse test axelar string lib

--- test/common/unit/Guardian.t.sol
+++ test/common/unit/Guardian.t.sol
@@ -9,6 +9,8 @@ import {IAxelarAdapter} from "src/common/interfaces/adapters/IAxelarAdapter.sol"
 import {IWormholeAdapter} from "src/common/interfaces/adapters/IWormholeAdapter.sol";
 import {Guardian, ISafe, IMultiAdapter, IRoot, IRootMessageSender} from "src/common/Guardian.sol";
 
+import {AxelarAddressToString} from "test/common/unit/AxelarAdapter.t.sol";
+
 import "forge-std/Test.sol";
 
 contract GuardianTest is Test {
@@ -274,24 +276,5 @@ contract GuardianWireAxelarAdapterTest is GuardianTest {
 
         vm.prank(address(adminSafe));
         guardian.wireAxelarAdapter(localAdapter, centrifugeId, axelarId, adapterStr);
-    }
-}
-
-// From https://github.com/axelarnetwork/axelar-gmp-sdk-solidity/blob/main/contracts/libs/AddressString.sol#L30C26-L45C6
-library AxelarAddressToString {
-    function toAxelarString(address address_) internal pure returns (string memory) {
-        bytes memory addressBytes = abi.encodePacked(address_);
-        bytes memory characters = "0123456789abcdef";
-        bytes memory stringBytes = new bytes(42);
-
-        stringBytes[0] = "0";
-        stringBytes[1] = "x";
-
-        for (uint256 i; i < 20; ++i) {
-            stringBytes[2 + i * 2] = characters[uint8(addressBytes[i] >> 4)];
-            stringBytes[3 + i * 2] = characters[uint8(addressBytes[i] & 0x0f)];
-        }
-
-        return string(stringBytes);
     }
 }
