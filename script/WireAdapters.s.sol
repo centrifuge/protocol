a8212fc4491c6c08539646953f4fc2c17b9ce30c
lemunozm <lemunozm@gmail.com>
2025-07-08 13:44:48 +0200

    fix imports

--- script/WireAdapters.s.sol
+++ script/WireAdapters.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {IAdapter} from "src/common/interfaces/IAdapter.sol";
 import {Guardian} from "src/common/Guardian.sol";
+import {IAdapter} from "src/common/interfaces/IAdapter.sol";
 import {IAxelarAdapter} from "src/common/interfaces/adapters/IAxelarAdapter.sol";
 import {IWormholeAdapter} from "src/common/interfaces/adapters/IWormholeAdapter.sol";
 
