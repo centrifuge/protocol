3c15bfdb1bf1825dab29b078937e303131f5e042
William Freudenberger <w.freude@icloud.com>
2025-07-16 11:50:39 +0200

    chore: add v3 rely to spell 1

--- test/spell/LinkShareTokenCommon.sol
+++ test/spell/LinkShareTokenCommon.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
+import {IAuth} from "src/misc/interfaces/IAuth.sol";
+
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
+
 import {ISpoke} from "src/spoke/interfaces/ISpoke.sol";
 import {IShareToken} from "src/spoke/interfaces/IShareToken.sol";
 
@@ -18,10 +21,11 @@ contract LinkShareTokenCommon {
     bool public done;
     string public constant description = "Link V2 share tokens to V3 system";
 
-    // Common addresses across all networks
     ISpoke public constant SPOKE = ISpoke(0xd30Da1d7F964E5f6C2D9fE2AAA97517F6B23FA2B);
 
-    // JTRSY configuration (exists on all networks with same addresses)
+    address public constant ROOT_ADDRESS = 0x7Ed48C31f2fdC40d37407cBaBf0870B2b688368f;
+    address public constant BALANCE_SHEET_ADDRESS = 0xBcC8D02d409e439D98453C0b1ffa398dFFb31fda;
+
     PoolId public constant JTRSY_POOL_ID = PoolId.wrap(4139607887);
     ShareClassId public constant JTRSY_SHARE_CLASS_ID = ShareClassId.wrap(0x97aa65f23e7be09fcd62d0554d2e9273);
     IShareToken public constant JTRSY_SHARE_TOKEN = IShareToken(0x8c213ee79581Ff4984583C6a801e5263418C4b86);
@@ -34,5 +38,9 @@ contract LinkShareTokenCommon {
 
     function execute() internal virtual {
         SPOKE.linkToken(JTRSY_POOL_ID, JTRSY_SHARE_CLASS_ID, JTRSY_SHARE_TOKEN);
+
+        IAuth(address(JTRSY_SHARE_TOKEN)).rely(ROOT_ADDRESS);
+        IAuth(address(JTRSY_SHARE_TOKEN)).rely(BALANCE_SHEET_ADDRESS);
+        IAuth(address(JTRSY_SHARE_TOKEN)).rely(address(SPOKE));
     }
 }
--- test/spell/LinkShareTokenEth.sol
+++ test/spell/LinkShareTokenEth.sol
@@ -1,24 +1,31 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.28;
 
-import {LinkShareTokenCommon, LinkShareTokenParams} from "./LinkShareTokenCommon.sol";
+import {IAuth} from "src/misc/interfaces/IAuth.sol";
+
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
+
 import {IShareToken} from "src/spoke/interfaces/IShareToken.sol";
+
+import {LinkShareTokenCommon} from "./LinkShareTokenCommon.sol";
 
 /// @title  LinkShareTokenEth
 /// @notice Ethereum-specific spell that links both JTRSY and JAAA tokens
 contract LinkShareTokenEth is LinkShareTokenCommon {
-    // JAAA configuration (only exists on Ethereum mainnet)
     PoolId public constant JAAA_POOL_ID = PoolId.wrap(158696445);
     ShareClassId public constant JAAA_SHARE_CLASS_ID = ShareClassId.wrap(0x57e1b211a9ce6306b69a414f274f9998);
     IShareToken public constant JAAA_SHARE_TOKEN = IShareToken(0x5a0F93D040De44e78F251b03c43be9CF317Dcf64);
 
     function execute() internal override {
-        // Link JTRSY (from parent)
+        // Link JTRSY and grant permissions
         super.execute();
 
-        // Link JAAA (Ethereum only)
+        // Link JAAA and grant permissions
         SPOKE.linkToken(JAAA_POOL_ID, JAAA_SHARE_CLASS_ID, JAAA_SHARE_TOKEN);
+
+        IAuth(address(JAAA_SHARE_TOKEN)).rely(ROOT_ADDRESS);
+        IAuth(address(JAAA_SHARE_TOKEN)).rely(BALANCE_SHEET_ADDRESS);
+        IAuth(address(JAAA_SHARE_TOKEN)).rely(address(SPOKE));
     }
 }
