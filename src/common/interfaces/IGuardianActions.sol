ffcf8a862f83776f18423a67683a08ab0a8e1439
Jeroen Offerijns <git@o6ns.com>
2025-07-16 11:49:30 +0200

    Fix interface issues, add codespell

--- src/common/interfaces/IGuardianActions.sol
+++ src/common/interfaces/IGuardianActions.sol
@@ -5,7 +5,7 @@ import {PoolId} from "src/common/types/PoolId.sol";
 import {AssetId} from "src/common/types/AssetId.sol";
 
 interface IHubGuardianActions {
-    /// @notice Creates a new pool. `msg.sender` will be the admin of the created pool.
+    /// @notice Creates a new pool.
     /// @param currency The pool currency. Usually an AssetId identifying by a ISO4217 code.
     function createPool(PoolId poolId, address admin, AssetId currency) external payable;
 }
