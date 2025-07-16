d6d0fbd25e6e5ee39fde40638c9245e0ffb36fa7
lemunozm <lemunozm@gmail.com>
2025-07-16 15:35:24 +0200

    add registerUSCD

--- test/integration/Integration.t.sol
+++ test/integration/Integration.t.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
+import {ERC20} from "src/misc/ERC20.sol";
+
 import {PoolId} from "src/common/types/PoolId.sol";
 import {ShareClassId} from "src/common/types/ShareClassId.sol";
 import {MAX_MESSAGE_COST as GAS} from "src/common/interfaces/IGasService.sol";
@@ -52,11 +54,26 @@ contract CentrifugeIntegrationTestWithUtils is CentrifugeIntegrationTest {
     PoolId POOL_A;
     ShareClassId SC_1;
 
+    // Extra deployment
+    ERC20 usdc;
+
     function setUp() public override {
         super.setUp();
 
         POOL_A = hubRegistry.poolId(LOCAL_CENTRIFUGE_ID, 1);
         SC_1 = shareClassManager.previewNextShareClassId(POOL_A);
+
+        // Extra deployment
+        vm.startPrank(ADMIN);
+        usdc = new ERC20(6);
+        usdc.file("name", "USD Coin");
+        usdc.file("symbol", "USDC");
+        vm.label(address(usdc), "usdc");
+    }
+
+    function registerUSDC() public {
+        vm.startPrank(FUNDED);
+        spoke.registerAsset{value: GAS}(LOCAL_CENTRIFUGE_ID, address(usdc), 0);
     }
 
     function createPool() public {
@@ -74,5 +91,9 @@ contract CentrifugeIntegrationTestWithUtils is CentrifugeIntegrationTest {
 contract _CentrifugeIntegrationTestWithUtilsTest is CentrifugeIntegrationTestWithUtils {
     function testCreatePool() public {
         createPool();
+    }
+
+    function testRegisterUSDC() public {
+        registerUSDC();
     }
 }
