b9064a217230d4d3c250df06e994712949feaa6c
Jeroen Offerijns <git@o6ns.com>
2025-07-20 18:52:58 +0200

    Add address checks

--- script/FullDeployer.s.sol
+++ script/FullDeployer.s.sol
@@ -32,6 +32,8 @@ contract FullDeployer is HubDeployer, ExtendedSpokeDeployer, AdaptersDeployer {
     {
         _preDeployFull(commonInput, adaptersInput, batcher);
         _postDeployFull(batcher);
+
+        if (commonInput.version == keccak256(abi.encodePacked(("3")))) _verifyMainnetAddresses();
     }
 
     function _preDeployFull(
@@ -54,6 +56,43 @@ contract FullDeployer is HubDeployer, ExtendedSpokeDeployer, AdaptersDeployer {
         removeHubDeployerAccess(batcher);
         removeExtendedSpokeDeployerAccess(batcher);
         removeAdaptersDeployerAccess(batcher);
+    }
+
+    function _verifyMainnetAddresses() internal {
+        require(address(root) == 0x7Ed48C31f2fdC40d37407cBaBf0870B2b688368f);
+        require(address(guardian) == 0xFEE13c017693a4706391D516ACAbF6789D5c3157);
+        require(address(gasService) == 0x295262f96186505Ce67c67B9d29e36ad1f9EAe88);
+        require(address(gateway) == 0x51eA340B3fe9059B48f935D5A80e127d587B6f89);
+        require(address(multiAdapter) == 0x457C91384C984b1659157160e8543adb12BC5317);
+        require(address(messageProcessor) == 0xE994149c6D00Fe8708f843dc73973D1E7205530d);
+        require(address(messageDispatcher) == 0x21AF0C29611CFAaFf9271C8a3F84F2bC31d59132);
+        require(address(poolEscrowFactory) == 0xD166B3210edBeEdEa73c7b2e8aB64BDd30c980E9);
+        require(address(hubRegistry) == 0x12044ef361Cc3446Cb7d36541C8411EE4e6f52cb);
+        require(address(accounting) == 0xE999a426D92c30fEE4f074B3a53071A6e935419F);
+        require(address(holdings) == 0x0261FA29b3F2784AF17874428b58d971b6652C47);
+        require(address(shareClassManager) == 0xe88e712d60bfd23048Dbc677FEb44E2145F2cDf4);
+        require(address(hubHelpers) == 0xA30D9E76a80675A719d835a74d09683AD2CB71EE);
+        require(address(hub) == 0x9c8454A506263549f07c80698E276e3622077098);
+        require(address(identityValuation) == 0x3b8FaE903a6511f9707A2f45747a0de3B747711f);
+        require(address(tokenFactory) == 0xC8eDca090b772C48BcE5Ae14Eb7dd517cd70A32C);
+        require(address(spoke) == 0xd30Da1d7F964E5f6C2D9fE2AAA97517F6B23FA2B);
+        require(address(balanceSheet) == 0xBcC8D02d409e439D98453C0b1ffa398dFFb31fda);
+        require(address(contractUpdater) == 0x8dD5a3d4e9ec54388dAd23B8a1f3B2159B2f2D85);
+        require(address(routerEscrow) == 0xB86B6AE94E6d05AAc086665534A73fee557EE9F6);
+        require(address(globalEscrow) == 0x43d51be0B6dE2199A2396bA604114d24383F91E9);
+        require(address(asyncRequestManager) == 0x58d57896EBbF000c293327ADf33689D0a7Fd3d9A);
+        require(address(syncManager) == 0x0D82d9fa76CFCd6F4cc59F053b2458665C6CE773);
+        require(address(asyncVaultFactory) == 0xE01Ce2e604CCe985A06FA4F4bCD17f1F08417BF3);
+        require(address(syncDepositVaultFactory) == 0x3568184784E8ACCaacF51A7F710a3DE0144E4f29);
+        require(address(vaultRouter) == 0xdbCcee499563D4AC2D3788DeD3acb14FB92B175D);
+        require(address(freezeOnlyHook) == 0xBb7ABFB0E62dfb36e02CeeCDA59ADFD71f50c88e);
+        require(address(fullRestrictionsHook) == 0xa2C98F0F76Da0C97039688CA6280d082942d0b48);
+        require(address(freelyTransferableHook) == 0xbce8C1f411484C28a64f7A6e3fA63C56b6f3dDDE);
+        require(address(redemptionRestrictionsHook) == 0xf0C36EFD5F6465D18B9679ee1407a3FC9A2955dD);
+        require(address(onOfframpManagerFactory) == 0xcb084F79e8AE54e1373130F4F7119214FCe972a9);
+        require(address(merkleProofManagerFactory) == 0xaBd3cDc17C15a9E7771876cE24aB10A8E722781d);
+        require(address(vaultDecoder) == 0x72B188c37bD8Eb002d0D9c63CCd77F2Ff71d272e);
+        require(address(circleDecoder) == 0x6fce63E718fED6E20bAa8179e313C24cbF2EDa24);
     }
 
     function run() public virtual {
