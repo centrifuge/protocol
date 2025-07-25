d481bbdac87bbaf6a8fb19c862b3892ecc8abdc5
Jeroen Offerijns <git@o6ns.com>
2025-07-25 11:14:16 +0200

    Integrate axelar

--- test/spell/RelinkV2Eth.sol
+++ test/spell/RelinkV2Eth.sol
@@ -55,6 +55,11 @@ contract RelinkV2Eth is RelinkV2Common {
         RestrictionManagerLike(0x4737C3f62Cc265e786b280153fC666cEA2fBc0c0);
     AxelarAdapterLike public constant V2_AXELAR_ADAPTER = AxelarAdapterLike(0x85bAFcAdeA202258e3512FFBC3E2c9eE6Ad56365);
 
+    bytes32 public constant COMMAND_ID = 0x373a36b10c3b4c2b0e6c1f7efe871cef762e96a72c68249094b05238b7a5efe0;
+    string public constant SOURCE_CHAIN = "centrifuge";
+    string public constant SOURCE_ADDR = "0x7369626CEF070000000000000000000000000000";
+    bytes public constant PAYLOAD = hex'1600000000097583fd57e1b211a9ce6306b69a414f274f9998491edfb0b8b608044e227225c715981a30f3a44e000000000000000145564d00b64fd1c3a60c260188389850000186a1000000000000000000002d79883d2000000000000000000000002d79883d2000';
+
     function execute() internal override {
         // Relink JTRSY and JAAAA
         _relink(USDC_TOKEN, JTRSY_SHARE_TOKEN, JTRSY_VAULT_ADDRESS);
@@ -77,9 +82,9 @@ contract RelinkV2Eth is RelinkV2Common {
         V2_RESTRICTION_MANAGER.updateMember(address(JAAA_SHARE_TOKEN), address(this), type(uint64).max);
 
         // Fulfill and burn shares
-        // V2_AXELAR_ADAPTER.execute();
-        V2_INVESTMENT_MANAGER.mint(JAAA_VAULT_ADDRESS, 0, address(this), JAAA_INVESTOR);
-        JAAA_SHARE_TOKEN.burn(address(this), 50_000_000e6);
+        V2_AXELAR_ADAPTER.execute(COMMAND_ID, SOURCE_CHAIN, SOURCE_ADDR, PAYLOAD);
+        V2_INVESTMENT_MANAGER.mint(JAAA_VAULT_ADDRESS, 50000000000000, address(this), JAAA_INVESTOR);
+        JAAA_SHARE_TOKEN.burn(address(this), 50000000000000);
 
         // Remove permissions on restriction manager from the spell
         V2_RESTRICTION_MANAGER.updateMember(address(JAAA_SHARE_TOKEN), address(this), 0);
