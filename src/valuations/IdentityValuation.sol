b819fc166e5b7700cb3976ec6e15544c7c4486ed
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2025-07-28 19:03:09 +0200

    Ref: valuations module (#541)
    * absolute imports
    
    * reorganize valuations module
    
    * imports to relative

--- src/valuations/IdentityValuation.sol
+++ src/valuations/IdentityValuation.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.28;
 
-import {d18} from "./types/D18.sol";
-import {MathLib} from "./libraries/MathLib.sol";
-import {IERC6909Decimals} from "./interfaces/IERC6909.sol";
 import {IIdentityValuation} from "./interfaces/IIdentityValuation.sol";
+
+import {d18} from "../misc/types/D18.sol";
+import {MathLib} from "../misc/libraries/MathLib.sol";
+import {IERC6909Decimals} from "../misc/interfaces/IERC6909.sol";
 
 import {AssetId} from "../common/types/AssetId.sol";
 import {BaseValuation} from "../common/BaseValuation.sol";
