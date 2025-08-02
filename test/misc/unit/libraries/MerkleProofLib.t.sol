4521884538fae8e9b04c88d04f757034c38c777f
Jeroen Offerijns <git@o6ns.com>
2025-08-02 14:53:48 +0200

    Add merkle proof lib

--- test/misc/unit/libraries/MerkleProofLib.t.sol
+++ test/misc/unit/libraries/MerkleProofLib.t.sol
@@ -7,18 +7,18 @@ import "forge-std/Test.sol";
 
 /// @author Modified from https://github.com/transmissions11/solmate/blob/main/src/test/MerkleProofLib.t.sol
 contract MerkleProofLibTest is Test {
-    function testVerifyEmptyMerkleProofSuppliedLeafAndRootSame() public view {
+    function testSymbolicVerifyEmptyMerkleProofSuppliedLeafAndRootSame() public view {
         bytes32[] memory proof;
         assertEq(this.verify(proof, 0x00, 0x00), true);
     }
 
-    function testVerifyEmptyMerkleProofSuppliedLeafAndRootDifferent() public view {
+    function testSymbolicVerifyEmptyMerkleProofSuppliedLeafAndRootDifferent() public view {
         bytes32[] memory proof;
         bytes32 leaf = "a";
         assertEq(this.verify(proof, 0x00, leaf), false);
     }
 
-    function testValidProofSupplied() public view {
+    function testSymbolicValidProofSupplied() public view {
         // Merkle tree created from leaves ['a', 'b', 'c'].
         // Leaf is 'a'.
         bytes32[] memory proof = new bytes32[](2);
@@ -29,7 +29,7 @@ contract MerkleProofLibTest is Test {
         assertEq(this.verify(proof, root, leaf), true);
     }
 
-    function testVerifyInvalidProofSupplied() public view {
+    function testSymbolicVerifyInvalidProofSupplied() public view {
         // Merkle tree created from leaves ['a', 'b', 'c'].
         // Leaf is 'a'.
         // Proof is same as testValidProofSupplied but last byte of first element is modified.
