513310d9d6ba9922fd34d8b12dc3dad6d304ea61
Jeroen Offerijns <git@o6ns.com>
2025-08-03 15:31:46 +0200

    Implement

--- src/managers/QueueManager.sol
+++ src/managers/QueueManager.sol
@@ -6,6 +6,7 @@ import {MathLib} from "../misc/libraries/MathLib.sol";
 import {IMulticall} from "../misc/interfaces/IMulticall.sol";
 
 import {PoolId} from "../common/types/PoolId.sol";
+import {AssetId} from "../common/types/AssetId.sol";
 import {ShareClassId} from "../common/types/ShareClassId.sol";
 
 import {IBalanceSheet} from "../spoke/interfaces/IBalanceSheet.sol";
@@ -13,24 +14,24 @@ import {IUpdateContract} from "../spoke/interfaces/IUpdateContract.sol";
 
 /// @dev minDelay can be set to a non-zero value, for cases where assets or shares can be permissionlessly modified
 ///      (e.g. if the on/off ramp manager is used, or if sync deposits are enabled). This prevents spam.
-contract QueueManager {
+contract QueueManager is IUpdateContract {
     using CastLib for *;
 
     error InvalidPoolId();
     error NotSpoke();
     error NoUpdates();
 
-    PoolId public immutable poolId;
-    ShareClassId public immutable scId;
     address public immutable contractUpdater;
     IBalanceSheet public immutable balanceSheet;
 
-    uint64 public minDelay;
-    uint64 public lastSync;
+    struct ShareClassState {
+        uint64 minDelay;
+        uint64 lastSync;
+    }
 
-    constructor(PoolId poolId_, ShareClassId scId_, address contractUpdater_, IBalanceSheet balanceSheet_) {
-        poolId = poolId_;
-        scId = scId_;
+    mapping(PoolId => mapping(ShareClassId => ShareClassState)) public sc;
+
+    constructor(address contractUpdater_, IBalanceSheet balanceSheet_) {
         contractUpdater = contractUpdater_;
         balanceSheet = balanceSheet_;
     }
@@ -41,7 +42,6 @@ contract QueueManager {
 
     /// @inheritdoc IUpdateContract
     function update(PoolId poolId_, ShareClassId scId_, bytes calldata payload) external {
-        require(poolId == poolId_, InvalidPoolId());
         require(msg.sender == contractUpdater, NotSpoke());
 
         // TODO: allow updating lastSync, extraGasLimit
@@ -51,53 +51,32 @@ contract QueueManager {
     // Sync
     //----------------------------------------------------------------------------------------------
 
-    function sync(uint32 maxAssetSubmissions) external {
-        require(lastSync == 0 || minDelay == 0 || block.timestamp >= lastSync + minDelay);
+    /// @dev It is the caller's responsibility to ensure all asset IDs have a non-zero delta,
+    ///      and `sync` is called n times up until the moment all asset IDs are included, and the shares
+    ///      get synced as well.
+    ///
+    ///      TODO: how to prevent spam for invalid asset IDs? Without checking the delta is non-negative,
+    ///      since this would open up a DoS vector.
+    function sync(PoolId poolId, ShareClassId scId, AssetId[] calldata assetIds) external {
+        ShareClassState storage sc_ = sc[poolId][scId];
+        require(sc_.lastSync == 0 || sc_.minDelay == 0 || block.timestamp >= sc_.lastSync + sc_.minDelay);
 
         (uint128 delta, bool isPositive, uint32 queuedAssetCounter, uint64 nonce) =
             balanceSheet.queuedShares(poolId, scId);
         require(delta > 0 || queuedAssetCounter > 0, NoUpdates());
 
-        bool submitShares = maxAssetSubmissions >= queuedAssetCounter;
-        uint256 submissions = MathLib.min(maxAssetSubmissions, queuedAssetCounter);
+        bool submitShares = assetIds.length >= queuedAssetCounter;
+        uint256 submissions = MathLib.min(assetIds.length, queuedAssetCounter);
         bytes[] memory cs = new bytes[](submitShares ? submissions + 1 : submissions);
-
         for (uint256 i; i < submissions; i++) {
-            cs[i + 1] = abi.encodeWithSelector(balanceSheet.submitQueuedAssets.selector, poolId, scId, assetId, 0);
+            cs[i] = abi.encodeWithSelector(balanceSheet.submitQueuedAssets.selector, poolId, scId, assetIds[i], 0);
         }
 
         if (submitShares) {
             cs[cs.length] = abi.encodeWithSelector(balanceSheet.submitQueuedShares.selector, poolId, scId, 0);
         }
+        sc_.lastSync = uint64(block.timestamp);
 
         IMulticall(address(balanceSheet)).multicall(cs);
-
-        lastSync = block.timestamp;
-    }
-
-    function sync() external {
-        // TODO: maxNumbSubmissions = queuedAssetCount
-    }
-}
-
-contract SyncManagerFactory is ISyncManagerFactory {
-    address public immutable contractUpdater;
-    IBalanceSheet public immutable balanceSheet;
-
-    constructor(address contractUpdater_, IBalanceSheet balanceSheet_) {
-        contractUpdater = contractUpdater_;
-        balanceSheet = balanceSheet_;
-    }
-
-    /// @inheritdoc ISyncManagerFactory
-    function newManager(PoolId poolId, ShareClassId scId) external returns (ISyncManager) {
-        require(address(balanceSheet.spoke().shareToken(poolId, scId)) != address(0), InvalidIds());
-
-        SyncManager manager = new SyncManager{salt: keccak256(abi.encode(poolId.raw(), scId.raw()))}(
-            poolId, scId, contractUpdater, balanceSheet
-        );
-
-        emit DeploySyncManager(poolId, scId, address(manager));
-        return ISyncManager(manager);
     }
 }
