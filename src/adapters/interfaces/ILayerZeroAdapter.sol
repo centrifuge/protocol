4ed055ef651c61f43623fba256ea3665b3ed9dc4
Jeroen Offerijns <git@o6ns.com>
2025-08-25 11:05:24 +0200

    Fix incorrect natspec

--- src/adapters/interfaces/ILayerZeroAdapter.sol
+++ src/adapters/interfaces/ILayerZeroAdapter.sol
@@ -43,14 +43,6 @@ interface ILayerZeroEndpointV2 {
     function quote(MessagingParams calldata _params, address _sender) external view returns (MessagingFee memory);
 
     /// @notice Send a LayerZero message to the specified address at a LayerZero endpoint specified by our chainId.
-    /// @param  _dstChainId                 the destination chain identifier
-    /// @param  _remoteAndLocalAddresses    remote address concated with local address packed into 40 bytes
-    /// @param  _payload                    a custom bytes payload to send to the destination contract
-    /// @param  _refundAddress              if the source transaction is cheaper than the amount of value passed, refund
-    ///                                     the additional amount to this address
-    /// @param  _zroPaymentAddress          the address of the ZRO token holder who would pay for the transaction
-    /// @param  _adapterParams              parameters for custom functionality. e.g. receive airdropped native gas from
-    ///                                     the relayer on destination
     function send(MessagingParams calldata _params, address _refundAddress)
         external
         payable
