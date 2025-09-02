e814990efcc16bc7260104544b2a42b178d0cc7b
Jeroen Offerijns <git@o6ns.com>
2025-09-02 15:51:39 +0200

    Start rewriting interface

--- src/valuations/IdentityValuation.sol
+++ src/valuations/IdentityValuation.sol
@@ -6,16 +6,29 @@ import {IIdentityValuation} from "./interfaces/IIdentityValuation.sol";
 import {d18} from "../misc/types/D18.sol";
 import {IERC6909Decimals} from "../misc/interfaces/IERC6909.sol";
 
+import {PoolId} from "../common/types/PoolId.sol";
 import {AssetId} from "../common/types/AssetId.sol";
-import {BaseValuation} from "../common/BaseValuation.sol";
+import {ShareClassId} from "../common/types/ShareClassId.sol";
 import {PricingLib} from "../common/libraries/PricingLib.sol";
 import {IValuation} from "../common/interfaces/IValuation.sol";
 
-contract IdentityValuation is BaseValuation, IIdentityValuation {
-    constructor(IERC6909Decimals erc6909, address deployer) BaseValuation(erc6909, deployer) {}
+import {IHubRegistry} from "../hub/interfaces/IHubRegistry.sol";
+
+contract IdentityValuation is IIdentityValuation {
+    IHubRegistry public immutable hubRegistry;
+
+    constructor(IHubRegistry hubRegistry_) {
+        hubRegistry = hubRegistry_;
+    }
 
     /// @inheritdoc IValuation
-    function getQuote(uint128 baseAmount, AssetId base, AssetId quote) external view returns (uint128 quoteAmount) {
-        return PricingLib.convertWithPrice(baseAmount, _getDecimals(base), _getDecimals(quote), d18(1e18));
+    function getQuote(PoolId poolId, ShareClassId scId, AssetId assetId, uint128 baseAmount)
+        external
+        view
+        returns (uint128 quoteAmount)
+    {
+        return PricingLib.convertWithPrice(
+            baseAmount, hubRegistry.decimals(assetId), hubRegistry.decimals(poolId), d18(1e18)
+        );
     }
 }
