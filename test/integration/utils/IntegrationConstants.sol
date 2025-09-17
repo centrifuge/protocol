f9fea3df9baff641ac66b15c18f60c25d027f01f
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2025-09-17 17:01:50 +0200

    root messages only from mainnet (#613)
    * schedule relay only from mainnet
    
    * extend check to other messages
    
    * constant inside contract

--- test/integration/utils/IntegrationConstants.sol
+++ test/integration/utils/IntegrationConstants.sol
@@ -10,10 +10,10 @@ import {MAX_MESSAGE_COST} from "../../../src/common/interfaces/IGasService.sol";
 /// @notice Centralized constants for integration tests
 library IntegrationConstants {
     // ======== Network IDs ========
-    uint16 constant CENTRIFUGE_ID_A = 5;
-    uint16 constant CENTRIFUGE_ID_B = 6;
-    uint16 constant CENTRIFUGE_ID_C = 7;
-    uint16 constant LOCAL_CENTRIFUGE_ID = 1;
+    uint16 constant CENTRIFUGE_ID_A = 1;
+    uint16 constant CENTRIFUGE_ID_B = 2;
+    uint16 constant CENTRIFUGE_ID_C = 3;
+    uint16 constant LOCAL_CENTRIFUGE_ID = 4;
 
     // ======== Amounts ========
     uint128 constant DEFAULT_USDC_AMOUNT = 1e12; // 1M USDC
