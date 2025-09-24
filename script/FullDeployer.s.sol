4f69794392c44cdbf6d27121fd37a2c6ec9eeced
Jeroen <1748621+hieronx@users.noreply.github.com>
2025-09-24 17:32:12 +0200

    Add `SpokeHandler`, remove `HubHelpers` (#626)
    * Set up spoke handler, remove hub helpers
    
    * Imports
    
    * Fix some tests
    
    * Fix relys
    
    * Fix remaining tests
    
    * Rename SpokeHandler => HubHandler
    
    * Update script/HubDeployer.s.sol
    
    Co-authored-by: William Freudenberger <w.freude@icloud.com>
    
    * Update script/HubDeployer.s.sol
    
    Co-authored-by: William Freudenberger <w.freude@icloud.com>
    
    * Add file tests, fix deployment tests
    
    ---------
    
    Co-authored-by: William Freudenberger <w.freude@icloud.com>

--- script/FullDeployer.s.sol
+++ script/FullDeployer.s.sol
@@ -34,6 +34,7 @@ contract FullActionBatcher is ExtendedHubActionBatcher, ExtendedSpokeActionBatch
 
         // Rely others
         report.batchRequestManager.rely(address(report.hub.hub));
+        report.batchRequestManager.rely(address(report.hub.hubHandler));
 
         // File methods
         report.batchRequestManager.file("hub", address(report.hub.hub));
@@ -286,8 +287,8 @@ contract FullDeployer is ExtendedHubDeployer, ExtendedSpokeDeployer, AdaptersDep
             "ShareClassManager address mismatch with mainnet"
         );
         require(
-            address(hubHelpers) == 0xA30D9E76a80675A719d835a74d09683AD2CB71EE,
-            "HubHelpers address mismatch with mainnet"
+            address(hubHandler) == 0xA30D9E76a80675A719d835a74d09683AD2CB71EE,
+            "HubHandler address mismatch with mainnet"
         );
         require(address(hub) == 0x9c8454A506263549f07c80698E276e3622077098, "Hub address mismatch with mainnet");
         require(
