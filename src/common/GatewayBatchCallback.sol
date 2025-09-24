73ccb890afe9007707cde2940574813395f12210
lemunozm <lemunozm@gmail.com>
2025-09-24 16:54:23 +0200

    extended implementation with interface

--- src/common/GatewayBatchCallback.sol
+++ src/common/GatewayBatchCallback.sol
@@ -8,29 +8,32 @@ import {ShareClassId} from "./types/ShareClassId.sol";
 
 import {Auth} from "../misc/Auth.sol";
 
-// Gateway trust on GatewayBatcher
-contract GatewayBatchCallback is Auth {
-    IGateway public gateway;
-    address public transient sender;
+import {IGatewayBatchCallback} from "./interfaces/IGatewayBatchCallback.sol";
 
-    error AlreadyBatching();
-    error CallFailedWithEmptyRevert();
+// Gateway trust on GatewayBatcher
+contract GatewayBatchCallback is Auth, IGatewayBatchCallback {
+    IGateway public gateway;
+    address public transient caller;
 
     constructor(IGateway gateway_, address deployer) Auth(deployer) {
         gateway = gateway_;
     }
 
-    function file(bytes32 what, address instance) external auth {
-        if (what == "gateway") gateway = IGateway(instance);
+    /// @inheritdoc IGatewayBatchCallback
+    function file(bytes32 what, address data) external auth {
+        if (what == "gateway") gateway = IGateway(data);
+        else revert FileUnrecognizedParam();
+        emit File(what, data);
     }
 
+    /// @inheritdoc IGatewayBatchCallback
     function withBatch(bytes memory data) external payable returns (uint256 cost) {
-        require(sender == address(0), AlreadyBatching());
+        require(caller == address(0), AlreadyBatching());
 
         gateway.startBatching();
-        sender = msg.sender;
+        caller = msg.sender;
 
-        (bool success, bytes memory returnData) = msg.sender.call(data);
+        (bool success, bytes memory returnData) = msg.sender.call{value: msg.value}(data);
         if (!success) {
             uint256 length = returnData.length;
             require(length != 0, CallFailedWithEmptyRevert());
@@ -40,7 +43,7 @@ contract GatewayBatchCallback is Auth {
             }
         }
 
-        sender = address(0);
+        caller = address(0);
         return gateway.endBatching();
     }
 }
