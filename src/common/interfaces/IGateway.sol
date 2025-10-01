4ef9e3a7c6aa505d575b3cf2db092a514188591a
lemunozm <lemunozm@gmail.com>
2025-10-01 16:19:44 +0200

    update QueueManager

--- src/common/interfaces/IGateway.sol
+++ src/common/interfaces/IGateway.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
-import {IAdapter} from "./IAdapter.sol";
-import {IGasService} from "./IGasService.sol";
 import {IMessageHandler} from "./IMessageHandler.sol";
 
 import {IRecoverable} from "../../misc/interfaces/IRecoverable.sol";
