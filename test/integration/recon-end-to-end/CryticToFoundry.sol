7bbc002b764cede5e5bd42f5fee802a5167af4e6
nican0r <94120714+nican0r@users.noreply.github.com>
2025-10-08 10:05:16 +0800

    chore: reproducers

--- test/integration/recon-end-to-end/CryticToFoundry.sol
+++ test/integration/recon-end-to-end/CryticToFoundry.sol
@@ -45,5 +45,42 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
 
     /// === Potential Issues === ///
 
+    // forge test --match-test test_property_assetQueueCounterConsistency_11 -vvv
+    function test_property_assetQueueCounterConsistency_11() public {
+        shortcut_deployNewTokenPoolAndShare(0, 1, false, false, true, false);
+
+        shortcut_deposit_queue_cancel(0, 0, 1, 1, 0, 0);
+
+        property_assetQueueCounterConsistency();
+    }
+
+    // forge test --match-test test_property_shareQueueFlipBoundaries_26 -vvv
+    function test_property_shareQueueFlipBoundaries_26() public {
+        shortcut_deployNewTokenPoolAndShare(0, 1, false, false, false, false);
+
+        shortcut_request_deposit(0, 0, 0, 0);
+
+        balanceSheet_issue(1);
+
+        property_shareQueueFlipBoundaries();
+    }
+
     /// === Categorized Issues === ///
+    // forge test --match-test test_asyncVault_maxMint_2 -vvv
+    // TODO: come back to this to refactor inline checks
+    function test_asyncVault_maxMint_2() public {
+        shortcut_deployNewTokenPoolAndShare(1, 1, true, false, false, false);
+
+        shortcut_deposit_sync(0, 1000344664829076477032757366550618);
+
+        asyncVault_maxMint(0, 0, 0);
+    }
+
+    // forge test --match-test test_doomsday_zeroPrice_noPanics_3 -vvv
+    // NOTE: doesn't return 0 for maxDeposit if there's a nonzero maxReserve set
+    function test_doomsday_zeroPrice_noPanics_3() public {
+        shortcut_deployNewTokenPoolAndShare(0, 1, false, false, false, false);
+
+        doomsday_zeroPrice_noPanics();
+    }
 }
