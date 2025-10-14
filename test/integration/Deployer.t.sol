fc0a9ddff84d20966eaa82d34c431d3938d7decc
Jeroen Offerijns <git@o6ns.com>
2025-10-14 15:59:07 +0200

    Remove gateway wards

--- test/integration/Deployer.t.sol
+++ test/integration/Deployer.t.sol
@@ -98,8 +98,6 @@ contract FullDeploymentTestCore is FullDeploymentConfigTest {
         vm.assume(nonWard != address(messageDispatcher));
         vm.assume(nonWard != address(messageProcessor));
         vm.assume(nonWard != address(spoke));
-        vm.assume(nonWard != address(balanceSheet));
-        vm.assume(nonWard != address(hub));
 
         assertEq(gateway.wards(address(root)), 1);
         assertEq(gateway.wards(address(protocolGuardian)), 1);
@@ -107,8 +105,6 @@ contract FullDeploymentTestCore is FullDeploymentConfigTest {
         assertEq(gateway.wards(address(messageDispatcher)), 1);
         assertEq(gateway.wards(address(messageProcessor)), 1);
         assertEq(gateway.wards(address(spoke)), 1);
-        assertEq(gateway.wards(address(balanceSheet)), 1);
-        assertEq(gateway.wards(address(hub)), 1);
         assertEq(gateway.wards(nonWard), 0);
 
         // dependencies set correctly
