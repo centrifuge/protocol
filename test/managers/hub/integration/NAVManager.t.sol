c9dab8b42d3630cb4f32bb172c975b986a3475f9
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2025-10-17 10:44:37 +0200

    import

--- test/managers/hub/integration/NAVManager.t.sol
+++ test/managers/hub/integration/NAVManager.t.sol
@@ -10,6 +10,7 @@ import {ShareClassId} from "../../../../src/core/types/ShareClassId.sol";
 import {AssetId, newAssetId} from "../../../../src/core/types/AssetId.sol";
 import {IValuation} from "../../../../src/core/hub/interfaces/IValuation.sol";
 import {ISnapshotHook} from "../../../../src/core/hub/interfaces/ISnapshotHook.sol";
+import {IShareClassManager} from "../../../../src/core/hub/interfaces/IShareClassManager.sol";
 
 import {INAVHook} from "../../../../src/managers/hub/interfaces/INAVManager.sol";
 
